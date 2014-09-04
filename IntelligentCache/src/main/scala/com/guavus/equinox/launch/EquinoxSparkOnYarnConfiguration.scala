eab8eee0f5bb02675ffe2a0165940c345712d9ca
archit.thakur <archit.thakur@guavus.com>
2014-09-05 04:15:16 +0530

    SPX-175: tomcat integration.

--- IntelligentCache/src/main/scala/com/guavus/equinox/launch/EquinoxSparkOnYarnConfiguration.scala
+++ IntelligentCache/src/main/scala/com/guavus/equinox/launch/EquinoxSparkOnYarnConfiguration.scala
@@ -5,8 +5,8 @@ import scala.collection.mutable.HashMap
 object EquinoxSparkOnYarnConfiguration {
 
   val config = new HashMap[String, Any]
-  override def get(key: String) = config.get(key)
-  override def set(key: String, value: Any) = config.put(key, value)
+  def get(key: String) = config.get(key)
+  def set(key: String, value: Any) = config.put(key, value)
 
   
 }
