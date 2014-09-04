33694c396baca83280567c6ea84b9d878083271c
archit.thakur <archit.thakur@guavus.com>
2014-09-05 00:24:37 +0530

    SPX-175: build dev

--- IntelligentCache/src/main/scala/com/guavus/equinox/launch/EquinoxLauncher.scala
+++ IntelligentCache/src/main/scala/com/guavus/equinox/launch/EquinoxLauncher.scala
@@ -18,6 +18,8 @@ import com.guavus.equinox.utils.CustomClasspathModificationEngine
 import java.io.File
 import com.guavus.equinox.common.EquinoxConstants
 import java.lang.RuntimeException
+import org.apache.catalina.startup.Tomcat
+import org.apache.catalina.Context
 
 class EquinoxLauncher extends HttpServlet {
   
@@ -38,13 +40,15 @@ class EquinoxLauncher extends HttpServlet {
     response.setContentType("text/html")
     val out = response.getWriter
 //    out.println("<h1>" + message + "</h1>")
-//    out.println("---------")
+    out.println("---------")
 //    out.println(q)
 //    val baseRdd = SparkLauncher.run(q, bool)
+    context.sqlContext.sql(request.getParameter("sqlQuery"))
 //    CustomClasspathModificationEngine.addFile(new File("/opt/hadoop"))
 //    val argument = "--class com.guavus.equinox.launch.Del --master yarn-client" /* --jars \" + FileWrapper.commaSeparatedFile("/data/archit/server_testing_scala/solution/WEB-INF/lib") + */ + " /data/archit/server_testing_scala/solution/WEB-INF/lib/IntelligentCache-0.1-SNAPSHOT-jar-with-guava-hive-dependencies.jar"
 //    SparkSubmit.main(argument.split(" "))
     
+    /*
     EquinoxConfiguration.Runmode.getValue() match { 
       
       case _ => { 
@@ -62,6 +66,9 @@ class EquinoxLauncher extends HttpServlet {
       }
       case x => throw new RuntimeException("The mode " + EquinoxConfiguration.Runmode.getValue() +" is not supported yet.")
     }
+    * 
+    * 
+    */
 //    for(td <- baseRdd.collect)
 //      out.println(td)
 //    out.println("-----")
@@ -77,6 +84,99 @@ class EquinoxLauncher extends HttpServlet {
       case x => throw new RuntimeException("The mode" + EquinoxConfiguration.Runmode.getValue() +" is not supported yet.")
     }
   }
+}
+object EquinoxLauncher { 
+  
+  case class SearchPRI_InteractionEgressMeasure(id: Long, ts: Long, TTS_B: Long)
+  case class SearchPRI_InteractionEgressDimension(id: Long, ts: Long, EgressProspectEntityId: Long, EgressNeighborEntityId: Long, EgressAS: Long, EgressIP: Long, EgressRTR: Long, OutgoingIF: Long, FlowDirection: Long)
+  case class searchIngressCustCubeDimension(id: Long, ts: Long, IngressCustomerEntityId: Long, IngressAS: Long, IngressIP: Long, IngressRTR: Long, IncomingIF: Long, IngressRuleId: Long, FlowDirection: Long)
+  case class searchIngressCustCubeMeasure(id: Long, ts: Long, TTS_B: Long, On_net_B: Long, Off_net_B: Long, Local_B: Long, Regional_B: Long, Continental_B: Long, XAtlantic_B: Long, XPacific_B: Long, XOthers_B: Long)
+  
+  def main(args: Array[String]): Unit = { 
+    
+    val sparkConf = new SparkConf
+  sparkConf.set("spark.app.name", "yarn-client")
+//  sparkConf.setMaster("yarn-cluster") 
+  val sparkContextEquinox = new SparkContext(sparkConf) //context.sparkContextEquinox
+//  FileWrapper.addLocalJar(sparkContextEquinox, "/data/archit/server_testing_scala/solution/WEB-INF/lib/")
+  val sqlContext = new SQLContext(sparkContextEquinox)
+      val orcFile1 = sparkContextEquinox.newAPIHadoopFile[NullWritable, OrcStruct, OrcNewInputFormat]("/data/intelligentcache/orc/searchIngressCustCubeDimension.orc")
+    val orcFile2 = sparkContextEquinox.newAPIHadoopFile[NullWritable, OrcStruct, OrcNewInputFormat]("/data/intelligentcache/orc/searchIngressCustCubeMeasure.orc")
+    import sqlContext._
+//    
+    val dimensionRdd = orcFile1.map(iSearchPRI_InteractionEgressDimension).registerAsTable("isearchIngressCustCubeDimension")
+    val measureRdd = orcFile2.map(iSearchPRI_InteractionEgressMeasure).registerAsTable("isearchIngressCustCubeMeasure")
+//    
+    cacheTable("isearchIngressCustCubeDimension")
+    cacheTable("isearchIngressCustCubeMeasure")
+
+    val x_ = sqlContext.sql("select id from isearchIngressCustCubeDimension")
+
+    val x$ = sparkContextEquinox.parallelize(List(1 to 10000), 2).map({ i =>
+      if(scala.math.random % 2 == 0)
+        0 else 1
+    }).reduce(_+_)
+    
+//    for(_$x <- x_.collect)
+//      println(_$x)
+    
+    sparkContextEquinox.stop();
+  
+//	  val tomcat = new Tomcat();
+//	  tomcat.setPort(38080);
+//	  val base: File = new File(System.getProperty("java.io.tmpdir"));
+////	  val rootCtx: Context = tomcat.addContext("/app", base.getAbsolutePath());
+////	  Tomcat.addServlet(rootCtx, "equinox", new EquinoxLauncher());
+////	  Tomcat.addServletMapping("/date", "dateServlet");
+//	  tomcat.addWebapp("/", new File("/data/archit/server_testing_scala/solution").getAbsolutePath())
+//	  
+//	  tomcat.start();
+//	  tomcat.getServer().await();
+////	  sparkContextEquinox.stop();
+  }
+  
+  def stringToLong(str: String) = {
+    
+    try{
+      str.toLong
+    } catch {
+      case ex: NumberFormatException => Int.MinValue 	
+    }
+  }
+   
+  def iSearchPRI_InteractionEgressDimension(tuple: (NullWritable, OrcStruct)) = { 
+      
+    val struct = tuple._2
+    val field = struct.toString.substring(1)
+    val l = field.length
+    val token = field.substring(0, field.length - 2).split(',').map(_.trim)
+    searchIngressCustCubeDimension(stringToLong(token(0)), stringToLong(token(1)), stringToLong(token(2)), stringToLong(token(3)), stringToLong(token(4)), stringToLong(token(5)), stringToLong(token(6)), stringToLong(token(7)), stringToLong(token(8)))
+  }
+  
+  def iSearchPRI_InteractionEgressMeasure(tuple: (NullWritable, OrcStruct)) = { 
+    val struct = tuple._2
+    val field = struct.toString.substring(1)
+    val l = field.length
+    val token = field.substring(0, field.length - 2).split(',').map(_.trim)
+    searchIngressCustCubeMeasure(stringToLong(token(0)), stringToLong(token(1)), stringToLong(token(2)), stringToLong(token(3)), stringToLong(token(4)), stringToLong(token(5)), stringToLong(token(6)), stringToLong(token(7)), stringToLong(token(8)), stringToLong(token(9)), stringToLong(token(10)))
+  }
+}
+
+object context{
+  val sparkConf = new SparkConf
+  sparkConf.set("spark.app.name", "yarn-client")
+  sparkConf.setMaster("yarn-cluster") 
+  val sparkContextEquinox = new SparkContext(sparkConf)
+  val sqlContext = new SQLContext(sparkContextEquinox)
+      val orcFile1 = sparkContextEquinox.newAPIHadoopFile[NullWritable, OrcStruct, OrcNewInputFormat]("/data/archit/orc/searchIngressCustCubeDimension.orc")
+    val orcFile2 = sparkContextEquinox.newAPIHadoopFile[NullWritable, OrcStruct, OrcNewInputFormat]("/data/archit/orc/searchIngressCustCubeMeasure.orc")
+    import sqlContext._
+    
+    val dimensionRdd = orcFile1.map(SparkLauncher.iSearchPRI_InteractionEgressDimension).registerAsTable("isearchIngressCustCubeDimension")
+    val measureRdd = orcFile2.map(SparkLauncher.iSearchPRI_InteractionEgressMeasure).registerAsTable("isearchIngressCustCubeMeasure")
+    
+    cacheTable("isearchIngressCustCubeDimension")
+    cacheTable("isearchIngressCustCubeMeasure")
 }
 
 object SparkLauncher {
