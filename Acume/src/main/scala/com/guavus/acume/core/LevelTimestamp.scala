899e67564a38dc1303853f24c55ec478aef681e4
archit.thakur <archit.thakur@guavus.com>
2014-09-19 19:23:02 +0530

    SPX-201: basic rubix features
    Former-commit-id: c3a862125f065f4fc3d3f7d0b5f1f684287345c8

--- Acume/src/main/scala/com/guavus/acume/core/LevelTimestamp.scala
+++ Acume/src/main/scala/com/guavus/acume/core/LevelTimestamp.scala
@@ -1,6 +1,26 @@
 package com.guavus.acume.core
 
-case class LevelTimestamp {
+case class LevelTimestamp(level: CacheLevel, timestamp:Long)
 
+class CacheLevel extends Enumeration { 
+  
+  type CacheLevel = Value
+  val Hourly = Value("1h")
+  val Weekly = Value("1w")
+  val BiWeekly = Value("2w")
+  val Monthly = Value("1m")
+  val Yearly = Value("1y")
+  
+  def getCacheLevel(name: String): Value = { 
+    
+    name match { 
+      
+      case "1h" => Hourly
+      case "1w" => Weekly
+      case "2w" => BiWeekly
+      case "1m" => BiWeekly
+      case "1y" => Yearly
+      case _ => throw new UnSupportedException("This CacheLevel is not supported.")
+    }
+  }
 }
-
