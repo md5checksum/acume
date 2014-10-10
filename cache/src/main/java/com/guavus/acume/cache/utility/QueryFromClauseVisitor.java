de6651be16fe6dec4af358de37aaf843c7863bfc
archit.thakur <archit.thakur@guavus.com>
2014-10-10 10:25:43 +0530

    SPX-205: corrected cmake file for build.
    Former-commit-id: 87cd032d8f55620167e7cc0ab22c01ab48499395

--- cache/src/main/java/com/guavus/acume/cache/utility/QueryFromClauseVisitor.java
+++ cache/src/main/java/com/guavus/acume/cache/utility/QueryFromClauseVisitor.java
@@ -10,31 +10,37 @@ import net.sf.jsqlparser.statement.select.SubJoin;
 import net.sf.jsqlparser.statement.select.SubSelect;
 
 public class QueryFromClauseVisitor extends AbstractVisitor {
-//
-//	List<Tuple> list = new LinkedList<Tuple>();
-//	boolean istimestamp = false;
-//	
-//	//majorly dependent on the implementation of QueryBuilder style.
-//	//todo implement other methods in next build.
-//	public QueryFromClauseVisitor(LinkedList<Tuple> list) {
-//		this.list = list;
-//	}
-//
-//	public void visit(Table tableName){
-//		Tuple k = new Tuple();
-//		k.setTableName(tableName.getName());
-////		list.add(tableName.getName());
-//	}
-//
-//	public void visit(SubSelect subSelect){
-////		Pair<Long,Long> pair = new Pair<Long, Long>(0l, 0l);
-//		PlainSelect plainSelect = (PlainSelect)subSelect.getSelectBody();
-//		plainSelect.getFromItem().accept(this);
-//		plainSelect.getWhere().accept(new QueryWhereClauseVisitor(pair));
-//	}
-//
-//	public void visit(SubJoin subjoin){
-//		FromItem leftItem = subjoin.getLeft();
-//		FromItem rightItem = subjoin.getJoin().getRightItem();
-//	}
+
+	List<Tuple> list_tl = new LinkedList<Tuple>();//table list.
+	List<Tuple> list_ti = new LinkedList<Tuple>();//timestamp list.
+	boolean istimestamp = false;
+	
+	//majorly dependent on the implementation of QueryBuilder style.
+	//todo implement other methods in next build.
+	public QueryFromClauseVisitor(LinkedList<Tuple> list_ti, LinkedList<Tuple> list_tl) {
+		this.list_tl = list_tl;this.list_ti = list_ti;
+	}
+
+	public void visit(Table tableName){
+		Tuple k = new Tuple();
+		k.setTableName(tableName.getName());
+		k.setStartTime(0l);
+		k.setEndTime(0l);
+		list_tl.add(k);
+	}
+
+	public void visit(SubSelect subSelect){
+		Tuple t = new Tuple();
+		PlainSelect plainSelect = (PlainSelect)subSelect.getSelectBody(); 
+		plainSelect.getWhere().accept(new QueryWhereClauseVisitor(t));
+		t.setTableName("");
+		list_ti.add(t);
+		plainSelect.getFromItem().accept(this);
+		plainSelect.getWhere().accept(new QueryWhereClauseVisitor(pair));
+	}
+
+	public void visit(SubJoin subjoin){
+		FromItem leftItem = subjoin.getLeft();
+		FromItem rightItem = subjoin.getJoin().getRightItem();
+	}
 }
