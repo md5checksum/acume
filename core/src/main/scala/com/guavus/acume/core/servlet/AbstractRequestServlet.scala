79b0b4fd3968412868fa16a9e92e236daa41744c
Kashish Jain <kashish.jain@guavus.com>
2014-10-29 13:03:56 +0530

    SPX-203: Adding servlets for accume apis
    Former-commit-id: 178a581b1733e758c37a17cdeef60cc9223ff923

---
+++ core/src/main/scala/com/guavus/acume/core/servlet/AbstractRequestServlet.scala
@@ -0,0 +1,42 @@
+package com.guavus.acume.core.servlet
+
+import com.guavus.acume.rest.api.RestService
+import com.guavus.acume.rest.beans.AggregateResponse
+import com.guavus.acume.rest.beans.TimeseriesResponse
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import javax.ws.rs.Path
+import javax.xml.bind.annotation.XmlRootElement
+import com.guavus.acume.rest.beans.SearchResponse
+import javax.servlet.ServletException
+
+abstract class AbstractRequestServlet extends HttpServlet {
+
+  val service = new RestService()
+  
+  override def doPost(req: HttpServletRequest, resp: HttpServletResponse) {
+    
+    val response = getResponse(req)
+    
+    var finalResponse :String = null
+	
+    if(response.isInstanceOf[AggregateResponse]) {
+	  finalResponse = AggregateResponse.gson.toJson(response)
+	} else if(response.isInstanceOf[TimeseriesResponse]) {
+	  finalResponse = TimeseriesResponse.gson.toJson(response)
+	} else if(response.isInstanceOf[SearchResponse]) {
+	  //search response
+	  finalResponse = TimeseriesResponse.gson.toJson(response)
+	} else {
+	  throw new ServletException("Invalid response");
+	}
+    
+	resp.getOutputStream().print(finalResponse)
+	resp.flushBuffer()
+    
+  }
+  
+  def getResponse(req: HttpServletRequest) : Serializable
+  
+}
