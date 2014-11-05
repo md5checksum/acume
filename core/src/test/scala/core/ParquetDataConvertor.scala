76754df8ad1c24344b0f2378c96056078bd7b856
Kashish Jain <kashish.jain@guavus.com>
2014-11-05 17:21:08 +0530

    SPX-223: Conversion logic to parquet format
    Former-commit-id: fba1b05dd3556a3c9eb2a2ed1a9510a1347d0c4b

---
+++ core/src/test/scala/core/ParquetDataConvertor.scala
@@ -0,0 +1,173 @@
+package core
+
+import java.io.File
+import java.io.FileInputStream
+import scala.Array.canBuildFrom
+import scala.collection.JavaConversions.asScalaBuffer
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.annotation.AlphaComponent
+import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.catalyst.expressions.Row
+import org.apache.spark.sql.catalyst.types.LongType
+import org.apache.spark.sql.catalyst.types.StructField
+import org.apache.spark.sql.catalyst.types.StructType
+import com.guavus.acume.cache.common.BaseCube
+import com.guavus.acume.cache.common.ConversionToSpark
+import com.guavus.acume.cache.common.DataType
+import com.guavus.acume.cache.common.Dimension
+import com.guavus.acume.cache.common.DimensionSet
+import com.guavus.acume.cache.common.FieldType
+import com.guavus.acume.cache.common.Measure
+import com.guavus.acume.cache.common.MeasureSet
+import com.guavus.acume.cache.gen.Acume
+import com.guavus.acume.cache.utility.InsensitiveStringKeyHashMap
+import javax.xml.bind.JAXBContext
+import com.guavus.crux.core.Fields
+import com.guavus.acume.cache.common.ConversionToCrux
+import com.guavus.crux.core.TextDelimitedScheme
+
+object ParquetDataConvertor {
+  
+  val dimensionMap = new InsensitiveStringKeyHashMap[Dimension]
+  val measureMap = new InsensitiveStringKeyHashMap[Measure]
+  val baseCubeMap = new InsensitiveStringKeyHashMap[BaseCube]
+  
+  def main(args: Array[String]) {
+	//args = srcFilepath, cubename,   
+    
+    loadXML("/Users/kashish.jain/Documents/work/githome/acume/cache/src/test/resources/cubedefinition.xml")
+	copyDirectory(args(0), args(1))
+  }
+  
+  def getRow(row: String) = Row.fromSeq(row.split("\t").toSeq)
+  
+  def getNewFile(inputpath: String, outputpath:String, cubename:String, ttype: String, sqlContext: SQLContext) = {
+    
+      //type == d or f 	
+      var sc = sqlContext.sparkContext
+      val cube = baseCubeMap.get(cubename).get
+      val d = cube.dimension.dimensionSet
+      val m = cube.measure.measureSet
+
+	  val (schema, fields, _datatype) = if(ttype == "d") { 
+	    (StructType(cube.dimension.dimensionSet.map(field => { 
+	      StructField(field.getName, ConversionToSpark.convertToSparkDataType(field.getDataType), true)
+	    }).toList), 
+	    new Fields((1.to(d.size).map(_.toString).toArray)), 
+	    d.map(x => ConversionToCrux.convertToCruxFieldDataType(x.getDataType)))
+	  }
+	  else { 
+	    (StructType(cube.measure.measureSet.map(field => { 
+	      StructField(field.getName, ConversionToSpark.convertToSparkDataType(field.getDataType), true)
+	    }).toList), 
+	    new Fields((1.to(m.size).map(_.toString).toArray)), 
+	    m.map(x => ConversionToCrux.convertToCruxFieldDataType(x.getDataType)))
+	  }
+      
+      val text = new TextDelimitedScheme(fields, "\t", _datatype.toArray)._getRdd(inputpath, sqlContext.sparkContext).map(x => Row.fromSeq(x.getValueArray.toSeq))
+    
+      sqlContext.applySchema(text, schema).saveAsParquetFile(outputpath)
+      
+  }
+  
+  def copyDirectory(filePath: String, cubeName: String) {
+   
+   val src = new File(filePath);
+   val dest = new File(filePath + "/../parquetInstabase")
+   
+   val sparkConf = new SparkConf
+   sparkConf.set("spark.master","local")
+   sparkConf.set("spark.app.name","local")
+   val sc = new SparkContext(sparkConf)
+   val sqlContext = new SQLContext(sc)
+   
+   if(!src.exists())
+     System.exit(0)
+   else {
+     copyFolder(src, dest, cubeName, sqlContext)
+   } 
+ }
+ 
+  def copyFolder(src: File, dest: File, cubeName: String, sqlContext: SQLContext) {
+    
+	if(src.isDirectory()){
+ 
+    	//if directory not exists, create it
+    	if(!dest.exists()){
+    	   dest.mkdir();
+    	   System.out.println("Directory copied from " 
+                + src + "  to " + dest);
+    	}
+ 
+    	//list all the directory contents
+    	var files = src.list()
+    	for (file <- files) {
+		  //construct the src and dest file structure
+		  copyFolder(new File(src, file), new File(dest, file), cubeName, sqlContext);
+		}
+ 
+    }else{
+      if(src.getAbsolutePath().indexOf("/d/") != -1) 
+    	getNewFile(src.getAbsolutePath(), dest.getAbsolutePath(), cubeName, "d", sqlContext)
+      else if (src.getAbsolutePath().indexOf("/f/") != -1)
+        getNewFile(src.getAbsolutePath(), dest.getAbsolutePath(), cubeName, "f", sqlContext)
+    }
+  }
+  
+  def loadXML(xml: String) = { 
+    
+    val jc = JAXBContext.newInstance("com.guavus.acume.cache.gen")
+    val unmarsh = jc.createUnmarshaller()
+    val dim_id = new Dimension("timestamp", DataType.ACLong, 0)
+    val dim_ts = new Dimension("id", DataType.ACLong, 0)
+    val m_id = new Measure("ts", DataType.ACLong, "none", 0)
+    val m_ts = new Measure("tupleid", DataType.ACLong, "none", 0)
+    val acumeCube = unmarsh.unmarshal(new FileInputStream(xml)).asInstanceOf[Acume]
+   
+    for(lx <- acumeCube.getFields().getField().toList) { 
+
+      val info = lx.getInfo.split(':')
+      val name = info(0).trim
+      val datatype = DataType.getDataType(info(1).trim)
+      val fitype = FieldType.getFieldType(info(2).trim)
+      val functionName = if(info.length<4) "none" else info(3).trim	
+      fitype match{
+        case FieldType.Dimension => 
+          dimensionMap.put(name.trim, new Dimension(name, datatype, 0))
+        case FieldType.Measure => 
+          measureMap.put(name.trim, new Measure(name, datatype, functionName, 0 ))
+      }
+    }
+    
+	for(c <- acumeCube.getCubes().getCube().toList) yield {
+	  val cubeName = c.getName().trim
+	  val fields = c.getFields().split(",").map(_.trim)
+	  val dimensionSet = scala.collection.mutable.Set[Dimension]()
+	  dimensionSet.+=(dim_id)
+	  dimensionSet.+=(dim_ts)
+	  val measureSet = scala.collection.mutable.Set[Measure]()
+	  measureSet.+=(m_id)
+	  measureSet.+=(m_ts)
+	    
+	  for(ex <- fields){
+	    val fieldName = ex.trim
+	    
+	    dimensionMap.get(fieldName) match{
+	      case Some(dimension) => 
+	        dimensionSet.+=(dimension)
+	      case None =>
+	        measureMap.get(fieldName) match{
+	          case None => throw new Exception("Field not registered.")
+	          case Some(measure) => measureSet.+=(measure)
+	        }
+	    }
+	  }
+	 
+	  val cube = BaseCube(cubeName, DimensionSet(dimensionSet.toList), MeasureSet(measureSet.toList))
+	  baseCubeMap.put(cubeName, cube)
+    }
+  }
+
+}
