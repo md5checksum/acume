4f8bb90010a157cd4484b001e14adc3c0faa4c4e
archit.thakur <archit.thakur@guavus.com>
2014-12-01 19:30:26 +0530

    SPX-254: timeseries having 0 timestamps in the output should result in 0 data rather than exception d table renamed cached issue.
    Former-commit-id: 6b4d5ebca3a501faadb32188c011928e330f6e56

--- cache/src/main/scala/com/guavus/acume/cache/core/AcumeTreeCache.scala
+++ cache/src/main/scala/com/guavus/acume/cache/core/AcumeTreeCache.scala
@@ -118,8 +118,16 @@ extends AcumeCache(acumeCacheContext, conf, cube) {
     val startTimeCeiling = cacheLevelPolicy.getCeilingToLevel(startTime, level)
     val endTimeFloor = cacheLevelPolicy.getFloorToLevel(endTime, level)
     val list = Utility.getAllIntervals(startTimeCeiling, endTimeFloor, level)
-    val intervals: MutableMap[Long, MutableList[Long]] = MutableMap(level -> list)
-    buildTableForIntervals(intervals, tableName, isMetaData)
+    if(!list.isEmpty) { 
+      
+      val intervals: MutableMap[Long, MutableList[Long]] = MutableMap(level -> list)
+      buildTableForIntervals(intervals, tableName, isMetaData)
+    }
+    else { 
+      
+      Utility.getEmptySchemaRDD(acumeCacheContext.sqlContext, cube).registerTempTable(tableName)
+      MetaData(Nil)
+    }
   }
   
   private def getVariableRetentionMap: SortedMap[Long, Int] = {
@@ -164,15 +172,16 @@ extends AcumeCache(acumeCacheContext, conf, cube) {
       }
       timeIterated
     }
+    if(!levelTime.isEmpty) {
     val schemarddlist = levelTime.flatten
     val dataloadedrdd = applySchema(schemarddlist.reduce(_.unionAll(_)), finalSchema)
     val baseMeasureSetTable = cube.cubeName + "MeasureSet" + getUniqueRandomeNo
     val joinDimMeasureTableName = baseMeasureSetTable + getUniqueRandomeNo
-//    dataloadedrdd.collect.map(println)
     dataloadedrdd.registerTempTable(baseMeasureSetTable)
     AcumeCacheUtility.dMJoin(acumeCacheContext.sqlContext, dimensionTable.tblnm, baseMeasureSetTable, joinDimMeasureTableName)
     val _$acumecache = AcumeCacheUtility.getSchemaRDD(acumeCacheContext, cube, joinDimMeasureTableName)
     acumeCacheContext.sqlContext.applySchema(_$acumecache, _$acumecache.schema).registerTempTable(tableName)
+    }
     val klist = timestamps.toList
     MetaData(klist)
   }
