fcb9a7013141ef4798ad78da87467460c12d31c8
archit.thakur <archit.thakur@guavus.com>
2014-12-12 09:24:31 +0530

    Re-199: ui isues resolved.
    Former-commit-id: 0bc2ad94ac07d17dbcea13904d006a5b457cca0b

---
+++ core/src/main/scala/com/guavus/rubix/query/remote/flex/AggregateResultSet.scala
@@ -0,0 +1,233 @@
+package com.guavus.rubix.query.remote.flex
+
+import java.io.Serializable
+import java.lang.reflect.Type
+import java.text.DecimalFormat
+import javax.xml.bind.annotation.XmlElement
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParseException
+import com.google.gson.reflect.TypeToken
+import AggregateResultSet._
+import scala.reflect.{BeanProperty, BooleanBeanProperty}
+import scala.collection.JavaConversions._
+import java.util.List
+import java.util.ArrayList
+
+/**
+ * Aggregate result set represent one row of acume aggregate result. 
+ */
+object AggregateResultSet {
+
+  object JsonAdaptor {
+
+    private var gson: Gson = new Gson()
+
+  }
+
+  class JsonAdaptor extends JsonDeserializer[AggregateResultSet] {
+
+	  private var listOfDouble: Type = new TypeToken[List[Double]]() {
+  }.getType
+  
+    override def deserialize(jsonElement: JsonElement, typeOfT: Type, context: JsonDeserializationContext): AggregateResultSet = {
+      val record = new ArrayList[Any]()
+      val json = jsonElement.asInstanceOf[JsonObject]
+      val measures = JsonAdaptor.gson.fromJson(json.get("measures"), listOfDouble)
+      val jsonArray = json.get("record").getAsJsonArray
+      for (jsonItem <- jsonArray) {
+        record.add(jsonItem.getAsString)
+      }
+      new AggregateResultSet(record, measures)
+    }
+  }
+}
+
+@SerialVersionUID(1L)
+class AggregateResultSet extends Serializable {
+
+  @BeanProperty
+  var record: List[Any] = _
+
+  @BeanProperty
+  var measures: List[Any] = _
+
+  def this(record: List[Any], measures: List[Any]) {
+    this()
+    this.record = record
+    this.measures = measures
+  }
+
+  override def toString(): String = {
+    val format = new DecimalFormat("#.000000")
+    val duplicateMeasureList = new ArrayList[String]()
+    for (i <- 0 until measures.size) {
+      duplicateMeasureList.add(format.format(measures.get(i)))
+    }
+    val builder = new StringBuilder()
+    builder.append("AggregateResultSet [measures=")
+    builder.append(duplicateMeasureList)
+    builder.append(", record=")
+    builder.append(record)
+    builder.append("]")
+    builder.toString
+  }
+
+  override def equals(obj: Any): Boolean = {
+    if (obj == null) {
+      return false
+    }
+    if (!(obj.isInstanceOf[AggregateResultSet])) {
+      return false
+    }
+    val resultSet = obj.asInstanceOf[AggregateResultSet]
+    if (resultSet == this) {
+      return true
+    }
+    if (resultSet.toString == this.toString) {
+      true
+    } else {
+      false
+    }
+  }
+
+/*
+Original Java:
+package com.guavus.acume.rest.beans;
+
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+
+|**
+ * Single Resulset 
+ * 
+ *
+ *|
+public class AggregateResultSet implements Serializable{
+	
+
+    |**
+	 * 
+	 *|
+	private static final long serialVersionUID = 1L;
+
+	|**
+	 * Ordered List of Dimension values . The order is defined by
+	 * {@link AggregateResponse.responseDimensions}
+	 *|
+	private List<Object> record ;
+	
+	|**
+	 * Ordered List of Measure values . The order is defined by
+	 * {@link AggregateResponse.responseMeasures}
+	 *|
+	private List<Object> measures ;
+	
+	public AggregateResultSet() {
+	}
+	
+	
+	public AggregateResultSet(List<Object> record, List<Object> measures) {
+		super();
+		this.record = record;
+		this.measures = measures;
+	}
+
+	
+	@XmlElement(type=ArrayList.class)
+	public List<Object> getRecord() {
+		return record;
+	}
+
+	public void setRecord(List<Object> record) {
+		this.record = record;
+	}
+
+	
+	@XmlElement(type=ArrayList.class)
+	public List<Object> getMeasures() {
+		return measures;
+	}
+
+	public void setMeasures(List<Object> measures) {
+		this.measures = measures;
+	}
+
+	@Override
+	public String toString() {
+	    DecimalFormat format = new DecimalFormat("#.000000");
+	    //format.applyPattern("#.######");
+	    List<String> duplicateMeasureList =  new ArrayList<String>();
+	    for (int i = 0; i < measures.size(); i++) {
+            duplicateMeasureList.add(format.format(measures.get(i)));
+        }
+		StringBuilder builder = new StringBuilder();
+		builder.append("AggregateResultSet [measures=");
+		builder.append(duplicateMeasureList);
+		builder.append(", record=");
+		builder.append(record);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	@Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof AggregateResultSet)) {
+            return false;
+        }
+
+        AggregateResultSet resultSet = (AggregateResultSet) obj;
+        if (resultSet == this) {
+            return true;
+        }
+        //This is slow. Only used for testing purpose
+        if (resultSet.toString().equals(this.toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+	public static class JsonAdaptor implements JsonDeserializer<AggregateResultSet>{
+	    private static Gson gson = new Gson();
+	    private static Type listOfDouble = new TypeToken<List<Double>>() {}.getType();
+        @Override
+        public AggregateResultSet deserialize(JsonElement jsonElement, Type typeOfT,
+            JsonDeserializationContext context) throws JsonParseException {
+            List<Object> record = new ArrayList<Object>();
+            JsonObject json = (JsonObject) jsonElement;
+            List<Object> measures = gson.fromJson(json.get("measures"), listOfDouble);
+            JsonArray jsonArray = json.get("record").getAsJsonArray();
+            for(JsonElement jsonItem: jsonArray)
+            {
+                record.add(jsonItem.getAsString());
+            }
+            return new AggregateResultSet(record, measures);
+        }   
+
+    }
+}
+
+*/
+}
---
+++ core/src/main/scala/com/guavus/rubix/query/remote/flex/SortOrder.scala
@@ -0,0 +1,74 @@
+package com.guavus.rubix.query.remote.flex
+
+import scala.reflect.BeanProperty
+import com.guavus.rubix.query.remote.flex.SortDirection._
+
+class SortOrder {
+
+  @BeanProperty
+  var dimensionName: String = _
+
+  @BeanProperty
+  var sortOrder: SortDirection = _
+
+  def this(dimensionName: String, sortOrder: SortDirection) {
+    this()
+    this.dimensionName = dimensionName
+    this.sortOrder = sortOrder
+  }
+
+  override def toString(): String = {
+    "SortOrder [dimensionName=" + dimensionName + ", sortOrder=" + sortOrder + "]"
+  }
+
+/*
+Original Java:
+|**
+ * 
+ *|
+package com.guavus.rubix.search;
+
+|**
+ * @author Akhil Swain
+ * 
+ *|
+public class SortOrder {
+
+	private String dimensionName;
+	private Order sortOrder;
+
+    public SortOrder() {
+    }
+
+	public SortOrder(String dimensionName, Order sortOrder) {
+		this.dimensionName = dimensionName;
+		this.sortOrder = sortOrder;
+	}
+
+	public String getDimensionName() {
+		return dimensionName;
+	}
+
+	public void setDimensionName(String dimensionName) {
+		this.dimensionName = dimensionName;
+	}
+
+	public Order getSortOrder() {
+		return sortOrder;
+	}
+
+	public void setSortOrder(Order sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "SortOrder [dimensionName=" + dimensionName + ", sortOrder="
+				+ sortOrder + "]";
+	}
+
+}
+
+*/
+}
