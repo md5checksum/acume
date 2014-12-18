f4dc05f1648e56f1f1965e82e7ab8d3da82a6f46
archit.thakur <archit.thakur@guavus.com>
2014-12-18 12:41:51 +0530

    RAN-202: Where Clause should be completely configurable.
    Former-commit-id: b3fb12dbf1421edceb1a7ea7d77d659dc42101c2

--- core/src/main/scala/com/guavus/rubix/query/remote/flex/MeasureFilterData.scala
+++ core/src/main/scala/com/guavus/rubix/query/remote/flex/MeasureFilterData.scala
@@ -34,71 +34,8 @@ class MultiFilter extends Serializable {
 @SerialVersionUID(-4945096459581562055L)
 @XmlRootElement
 class MeasureFilterData extends Serializable {
-
+  
   @BeanProperty
   var filters: ArrayList[MultiFilter] = _
-
-  /*
-Original Java:
-package com.guavus.rubix.query.remote.flex;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement
-public class MeasureFilterData implements Serializable {
-
-	private static final long serialVersionUID = -4945096459581562055L;
-
-	private List<MultiFilter> filters;
-	
-	@XmlRootElement
-	public class MultiFilter implements Serializable {
-		
-		private static final long serialVersionUID = -2622636580887371088L;
-		List<MeasureSingleFilter> singleFilters;
-		private String measure;
-		
-		public MultiFilter() {
-		}
-		
-		public MultiFilter(String measure, List<MeasureSingleFilter> singleFilters) {
-			this.measure = measure;
-			this.singleFilters = singleFilters;
-		}
-		
-		@XmlElement(type=ArrayList.class)
-		public List<MeasureSingleFilter> getSingleFilters() {
-			return singleFilters;
-		}
-
-		public void setSingleFilters(List<MeasureSingleFilter> singleFilters) {
-			this.singleFilters = singleFilters;
-		}
-
-		public String getMeasure() {
-			return measure;
-		}
-
-		public void setMeasure(String measure) {
-			this.measure = measure;
-		}
-	}
-
-	@XmlElement(type=ArrayList.class)
-	public List<MultiFilter> getFilters() {
-		return filters;
-	}
-
-	public void setFilters(List<MultiFilter> filters) {
-		this.filters = filters;
-	}
-
-}
-
-*/
+  
 }
