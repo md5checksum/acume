4b62bfe7514dce4fbb28d96418b32ee9729f0eb8
pankaj.arora <pankaj.arora@guavus.com>
2014-12-29 12:23:53 +0530

    SPX-205: Added bin source and granularity
    Former-commit-id: 535f9bfdaa0db2dfc1ca7ff405c8ff0a2b2e7c0f

--- core/src/main/scala/com/guavus/acume/core/converter/Cube.scala
+++ core/src/main/scala/com/guavus/acume/core/converter/Cube.scala
@@ -4,10 +4,17 @@ import com.guavus.querybuilder.cube.schema.ICube
 import com.guavus.querybuilder.cube.schema.IField
 import scala.collection.JavaConversions._
 
-class Cube(cubeName : String, fields : List[IField]) extends ICube {
+class Cube(cubeName: String, fields: List[IField], binSource: String, granularity: Long) extends ICube {
 
-  def  getFields() : java.util.List[IField] = fields
-  
+  def getFields(): java.util.List[IField] = fields
+
   def getCubeName() = cubeName
-  
+
+  def getBinSourceValue(): String = {
+    binSource
+  }
+
+  def getTimeGranularityValue(): java.lang.Long = {
+    granularity
+  }
 }
