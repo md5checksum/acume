a0530f1ab17183bbc5bcd52cdb02ed3fa1971884
archit.thakur <archit.thakur@guavus.com>
2015-01-09 17:27:04 +0530

    SPX-304:[Acume] pergen configuration required to avoid out of memory scenarios of permgen.
    Conflicts:
    
    	Profiles/Default/rubix_acume_template.xml
    
    
    Former-commit-id: ac3f2d315fdcc2802157e51fd7a19faf38dc9b57

--- war/src/main/tomcat/bin/setenv.sh
+++ war/src/main/tomcat/bin/setenv.sh
@@ -4,4 +4,6 @@ else
         DEBUG_OPTS=" -Xdebug -Xrunjdwp:transport=dt_socket,address=$CLI_REPLACE_DEBUGPORT$,server=y,suspend=n"
 fi
 
-export ACUME_JAVA_OPTS=$DEBUG_OPTS
+ACUME_PERMGEN_CONFIG=" -XX:PermSize=$CLI_REPLACE_PERMSIZE$ -XX:MaxPermSize=$CLI_REPLACE_MAXPERMSIZE$ "
+export ACUME_JAVA_OPTS=$DEBUG_OPTS$ACUME_PERMGEN_CONFIG
+
