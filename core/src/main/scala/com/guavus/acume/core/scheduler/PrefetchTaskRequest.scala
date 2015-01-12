0344109f78b7358c2bed50a944c5fc5b15ea4285
pankaj.arora <pankaj.arora@guavus.com>
2015-01-12 12:37:24 +0530

    SPX-296: Corrected prefetchtaskResuest to sql method
    Former-commit-id: e7ff2ace8c9f6a9f766947966e3e04b4ddb2562d

--- core/src/main/scala/com/guavus/acume/core/scheduler/PrefetchTaskRequest.scala
+++ core/src/main/scala/com/guavus/acume/core/scheduler/PrefetchTaskRequest.scala
@@ -8,8 +8,9 @@ import java.util.ArrayList
 import com.guavus.rubix.query.remote.flex.SortDirection
 import scala.collection.JavaConverters._
 import scala.collection.JavaConversions._
+import com.guavus.rubix.query.remote.flex.NameValue
 
-class PrefetchTaskRequest extends QueryRequest {
+class PrefetchTaskRequest {
 
   @BeanProperty
   var queryRequest: QueryRequest = _
@@ -35,12 +36,12 @@ class PrefetchTaskRequest extends QueryRequest {
     builder.toString
   }
   
-  override def toSql(ts1: String): String = {
+  def toSql(ts1: String): String = {
     val columns = new ArrayList[String]()
-    columns.addAll(responseDimensions)
-    columns.addAll(responseMeasures)
-    if (cubeContextDimensions != null) {
-      for (dimension <- cubeContextDimensions) {
+    columns.addAll(queryRequest.responseDimensions)
+    columns.addAll(queryRequest.responseMeasures)
+    if (queryRequest.cubeContextDimensions != null) {
+      for (dimension <- queryRequest.cubeContextDimensions) {
         columns.add("c." + dimension)
       }
     }
@@ -78,25 +79,84 @@ class PrefetchTaskRequest extends QueryRequest {
     }
     
     val whereClause = 
-      checkflag(subQuery == null, "", " (placeholder) in (" + (if(subQuery != null)subQuery.toSql("")) + ") ") + 
-      checkflag(startTime < 0, "", " startTime = " + startTime) + 
-      checkflag(endTime < 0 , "" , " endTime = " + endTime) + getfilterstring + 
-      checkflag((paramMap == null || paramMap.size == 0), "", calculateParams(queryRequest.getParamMap)) + 
-      checkflag((responseFilters == null || responseFilters.size == 0), "", rf) + 
-      checkflag(binSource == null, "", " binSource " + " = '" + binSource + "' ") + 
-      checkflag(timeGranularity < 0, "", " timeGranularity = " + timeGranularity) +
+      checkflag(queryRequest.subQuery == null, "", " (placeholder) in (" + (if(queryRequest.subQuery != null)queryRequest.subQuery.toSql("")) + ") ") + 
+      checkflag(queryRequest.startTime < 0, "", " startTime = " + queryRequest.startTime) + 
+      checkflag(queryRequest.endTime < 0 , "" , " endTime = " + queryRequest.endTime) + getfilterstring + 
+      checkflag((queryRequest.paramMap == null || queryRequest.paramMap.size == 0), "", calculateParams(queryRequest.getParamMap)) + 
+      checkflag((queryRequest.responseFilters == null || queryRequest.responseFilters.size == 0), "", rf) + 
+      checkflag(queryRequest.binSource == null, "", " binSource " + " = '" + queryRequest.binSource + "' ") + 
+      checkflag(queryRequest.timeGranularity < 0, "", " timeGranularity = " + queryRequest.timeGranularity) +
       checkflag(false, "", " acumeQueryType = " + "scheduler") +
-      checkflag(searchRequest == null, "", " (placeholder) in (" + (if(searchRequest!= null) searchRequest.toSql()) + ") ")
+      checkflag(queryRequest.searchRequest == null, "", " (placeholder) in (" + (if(queryRequest.searchRequest!= null) queryRequest.searchRequest.toSql()) + ") ")
 
     val wherestring =
       (if (!whereClause.equals("")) " where " + whereClause else "") +
-        (if (sortProperty == null || sortProperty.isEmpty) "" else " order by " + sortProperty + " ") +
-        (if (sortDirection == null) "" else if (sortDirection == SortDirection.ASC.toString) " asc" else " desc") +
-        (if (length == -1) "" else "  limit " + length) +
-        (if (offset == 0) "" else " offset " + offset + " ")
+        (if (queryRequest.sortProperty == null || queryRequest.sortProperty.isEmpty) "" else " order by " + queryRequest.sortProperty + " ") +
+        (if (queryRequest.sortDirection == null) "" else if (queryRequest.sortDirection == SortDirection.ASC.toString) " asc" else " desc") +
+        (if (queryRequest.length == -1) "" else "  limit " + queryRequest.length) +
+        (if (queryRequest.offset == 0) "" else " offset " + queryRequest.offset + " ")
       
     var abs = "select " + ts1 + columns.toString.substring(1, columns.toString.length - 1) + " from global " +wherestring
     abs
+  }
+  
+   protected def calculateParams(params: Traversable[NameValue]): String = {
+    if(queryRequest.paramMap == null)
+      return "";
+    var sql = " "
+    for (nameValue <- params) {
+      sql += nameValue.toSql() + " AND "
+    }
+    if (!sql.equalsIgnoreCase(" ")) sql = sql.substring(0, sql.length - 4)
+    sql += " "
+    sql
+  }
+
+  protected def calculateResponseFilters(): String = {
+    var sql = " "
+    for (nameValue <- queryRequest.responseFilters) {
+      sql += nameValue.toSql() + " AND "
+    }
+    if(!sql.equals(" ")) 
+      sql = sql.substring(0, sql.length - 4)
+    sql += " "
+    sql
+  }
+
+  protected def calculateDimensionFilterData(): String = {
+    
+    if(queryRequest.filterData == null || queryRequest.filterData.isEmpty)
+      return "";
+    var sql = " ("
+    for (filter <- queryRequest.filterData) {
+      sql += "("
+      sql += filter.toSql
+      sql += ") or "
+    }
+    sql = sql.substring(0, sql.length - 3)
+    sql += ")"
+    sql
+  }
+  
+  protected def calculateDimensionFilters(): String = {
+
+    if (queryRequest.filters == null || queryRequest.filters.size == 0)
+      return "";
+    if (queryRequest.filters.size == 1) {
+      return calculateParams(queryRequest.filters.get(0))
+    }
+    var sql = " ("
+    for (filter <- queryRequest.filters) {
+      sql += "("
+      for (nameValue <- filter) {
+        sql += nameValue.toSql() + " AND "
+      }
+      sql = sql.substring(0, sql.length - 4)
+      sql += ") or "
+    }
+    sql = sql.substring(0, sql.length - 3)
+    sql += ")"
+    sql
   }
 
 }
