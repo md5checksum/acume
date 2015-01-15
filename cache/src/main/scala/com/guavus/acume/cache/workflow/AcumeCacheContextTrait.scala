43d82a76558f3034a86233188f93614b11586e31
archit.thakur <archit.thakur@guavus.com>
2015-01-15 18:17:30 +0530

    SPX-320:Add binsource to global property level. There should be a provision of not configuring binsource in cubedefinition.xml.
    Former-commit-id: 6e5e14b3c6bca36f3d31c18c2d176efc10cfa5ea

--- cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeCacheContextTrait.scala
+++ cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeCacheContextTrait.scala
@@ -11,7 +11,7 @@ trait AcumeCacheContextTrait extends Serializable {
 
   private [acume] def getCubeList: List[Cube]
   def isDimension(name: String): Boolean 
-  private [acume] def getFieldsForCube(name: String): List[String]
+  private [acume] def getFieldsForCube(name: String, binsource: String): List[String]
   private [acume] def getAggregationFunction(stringname: String) : String
   def getDefaultValue(fieldName: String): Any
   private [acume] def getCubeListContainingFields(lstfieldNames: List[String]): List[Cube]
