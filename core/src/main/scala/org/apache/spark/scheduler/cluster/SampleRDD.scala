82a630952e0698fce50ce92332985f2cc46cec33
Kashish Jain <kashish.jain@guavus.com>
2015-03-10 11:26:43 +0530

    SPX-388: Distributing partitions equally to all executors Zipping partitions
    Former-commit-id: 58c460c9de184fd98428aec5744db21e11a491ee

--- core/src/main/scala/org/apache/spark/scheduler/cluster/SampleRDD.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/SampleRDD.scala
@@ -1,45 +1,110 @@
 package org.apache.spark.scheduler.cluster
 
-import scala.Array.fallbackCanBuildFrom
 import scala.collection.Iterator
+import scala.collection.immutable.TreeMap
+import scala.collection.mutable.HashMap
 import scala.reflect.ClassTag
+
 import org.apache.spark.Partition
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.TaskContext
-import org.apache.spark.rdd.MappedRDD
-import org.apache.spark.rdd.RDD
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.rdd.HadoopRDD
-import org.apache.hadoop.io.LongWritable
-import org.apache.hadoop.io.Text
 
 class SampleRDD[T: ClassTag](@transient sc: SparkContext, numPartitions : Int) extends RDD[T](sc.emptyRDD) {
-
   
   var partitions1 : Array[Partition] = _
+  
+  val executorMapping  : TreeMap[String, String] = getExecutorData
+  
   override def getPartitions: Array[Partition] = {
     partitions1 = (for( i <- 0 until numPartitions) yield {
       new SamplePartition(i)
     }).toArray
     partitions1
   }
-//
-  override def getPreferredLocations(split: Partition): Seq[String] = {
-    val schedulerBackendField = sc.getClass().getDeclaredField("schedulerBackend")
-    schedulerBackendField.setAccessible(true)
-    val schedulerBackend = schedulerBackendField.get(sc)
-    println(schedulerBackend.getClass())
-    val executorDataMapField = classOf[CoarseGrainedSchedulerBackend].getDeclaredField("org$apache$spark$scheduler$cluster$CoarseGrainedSchedulerBackend$$executorDataMap")
-    executorDataMapField.setAccessible(true)
-    val executorData = executorDataMapField.get(schedulerBackend).asInstanceOf[scala.collection.mutable.HashMap[String, ExecutorData]].valuesIterator.map(x => x.executorHost).toArray
-    Array(executorData(split.index%executorData.size))
-  }
+  
 
+  override def getPreferredLocations(split: Partition): Seq[String] = {
+    val executorData = getExecutorData()
+    if(executorData.isEmpty)
+     return Seq()
+      
+    val executorId = (split.index % executorMapping.keySet.size + 1).toString
+    println("Executor id = " + executorId)
+    
+    if(isExecutorDead(executorData, executorId)){
+      var executorDataArray = executorData.values.toArray
+      println("partition " + split.index + " is dead. Executor = " + executorDataArray(split.index % executorData.size))
+      Array(executorDataArray(split.index % executorData.size))
+    } else {
+      println("partition " + split.index + " is alive. Executor = " + executorMapping.get(executorId).get)
+      Array(executorMapping.get(executorId).get)
+    }
+  }
+  
   override def compute(splitIn: Partition, context: TaskContext): Iterator[T] = {
     Iterator.empty
   }
+  
+  private def zipRdd[U: ClassTag](other: RDD[U]): RDD[U] = {
+    zipPartitions(other, preservesPartitioning = false) { (thisIter, otherIter) =>
+      new Iterator[U] {
+        def hasNext = otherIter.hasNext
+        def next = otherIter.next
+      }
+    }
+  }
+  
+  private def isExecutorDead(executorData : TreeMap[String, String], executorId : String) : Boolean = {
+    if(executorData.keySet.contains(executorId))
+      return false
+    true
+  }
+  
+//  private def getExecutorData2() = {
+//    val schedulerBackendField = sc.getClass().getDeclaredField("schedulerBackend")
+//    schedulerBackendField.setAccessible(true)
+//    val schedulerBackend = schedulerBackendField.get(sc)
+//    
+//    var executorData = new TreeMap[String, String]()
+//    println("=========== GetExecutorData called with scheduler class = " + schedulerBackend.getClass())
+//    if(schedulerBackend.isInstanceOf[CoarseGrainedSchedulerBackend]) {
+//	  val executorDataMapField = classOf[CoarseGrainedSchedulerBackend].getDeclaredField("org$apache$spark$scheduler$cluster$CoarseGrainedSchedulerBackend$$executorDataMap")
+//	  executorDataMapField.setAccessible(true)
+//	    
+//	  while(executorData.isEmpty)
+//	    executorDataMapField.get(schedulerBackend).asInstanceOf[HashMap[String, ExecutorData]].map(x => executorData+=(x._1 -> x._2.executorHost))
+//	    printExecutorData(executorData)
+//    }
+//    executorData
+//  }
+  
+  private def getExecutorData() = {
+    val schedulerBackendField = sc.getClass().getDeclaredField("schedulerBackend")
+    schedulerBackendField.setAccessible(true)
+    val schedulerBackend = schedulerBackendField.get(sc)
+    
+    var executorData = new TreeMap[String, String]()
+    println("=========== GetExecutorData called with scheduler class = " + schedulerBackend.getClass())
+    if(schedulerBackend.isInstanceOf[CoarseGrainedSchedulerBackend]) {
+	  val executorDataMapField = classOf[CoarseGrainedSchedulerBackend].getDeclaredField("org$apache$spark$scheduler$cluster$CoarseGrainedSchedulerBackend$$executorDataMap")
+	  executorDataMapField.setAccessible(true)
+	    
+	  while(executorData.isEmpty)
+	    executorDataMapField.get(schedulerBackend).asInstanceOf[HashMap[String, ExecutorData]].map(x => executorData+=(x._1 -> x._2.executorHost))
+	    executorData.-=("1")
+	    printExecutorData(executorData)
+    }
+    executorData
+  }
+  
+  private def printExecutorData(executorData : TreeMap[String, String]) {
+    println("======= printing executorData")
+    executorData.foreach(x => println(x._1 + " " + x._2))
+  }
+  
 }
 
 
@@ -48,9 +113,26 @@ object SampleRDD {
   def main(args: Array[String]) {
 	val conf = new SparkConf
     conf.set("spark.app.name", "yarn-client")
+    conf.set("spark.executor.instances", args(0))
+    
+    println(" =============== SQL is " + args(1))
+    println(" =============== num of executors " + args(0))
+    
     val sc = new SparkContext(conf)
-    val rdd = new SampleRDD(sc, 4)
-	rdd.getPartitions.map(rdd.getPreferredLocations(_)).map(_.map(println))
+	val hiveContext = new HiveContext(sc)
+	
+	val hiveRdd = hiveContext.sql(args(1))
+	val numPartitions = hiveRdd.partitions.size
+	println(" ======== NumPartitons = " + numPartitions)
+    
+	var sampleRdd = new SampleRDD(sc, numPartitions)
+	sampleRdd = sampleRdd.cache
+
+	val zipped = sampleRdd.zipRdd(hiveRdd)
+
+	println(" ========== printing zippedRdd")
+	zipped.collect.foreach(println)
+
   }
   
 }
