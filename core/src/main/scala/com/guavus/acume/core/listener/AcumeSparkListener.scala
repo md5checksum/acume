c7980eda271cbcae0b0660cf7363fc820a885f5d
pankaj.arora <pankaj.arora@guavus.com>
2015-03-12 17:16:31 +0530

    SPX-400: Added block manager removal listener for scheduler restart
    Former-commit-id: 329902e800be4e313d29f74f4c98291d4d3c0835

--- core/src/main/scala/com/guavus/acume/core/listener/AcumeSparkListener.scala
+++ core/src/main/scala/com/guavus/acume/core/listener/AcumeSparkListener.scala
@@ -4,6 +4,9 @@ import org.apache.spark.scheduler.SparkListener
 import org.apache.spark.scheduler.SparkListenerApplicationEnd
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.apache.spark.scheduler.SparkListenerBlockManagerRemoved
+import com.guavus.acume.core.configuration.ConfigFactory
+import com.guavus.acume.core.scheduler.QueryRequestPrefetchTaskManager
 
 class AcumeSparkListener extends SparkListener{
   
@@ -14,5 +17,14 @@ class AcumeSparkListener extends SparkListener{
     logger.error("Killing acume. Shutting down")
     System.exit(1)
   }
+}
 
+class AcumeBlockManagerRemovedListener extends SparkListener {
+  
+  private var logger: Logger = LoggerFactory.getLogger(classOf[AcumeSparkListener])
+  
+  override def onBlockManagerRemoved(blockManagerRemoved : SparkListenerBlockManagerRemoved) {
+    logger.info("Block manager {} removed from spark application at : {}", blockManagerRemoved.blockManagerId, blockManagerRemoved.time/1000)
+    ConfigFactory.getInstance.getBean(classOf[QueryRequestPrefetchTaskManager]).restartPrefetchScheduler
+  }
 }
