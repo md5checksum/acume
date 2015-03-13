fbf12c57f7f4a85df51f85194a579d2240fa5364
Kashish Jain <kashish.jain@guavus.com>
2015-03-13 10:47:01 +0530

    SPX-388: Improving the algorithm to distribute the partitions among executors. Handling the case where executor dies
    Former-commit-id: 8c719ed8ece7bb68975017bfc24e021ce4f98ff0

---
+++ core/src/test/scala/com/guavus/acume/core/scheduler/cluster/PartitionDistributorTest.scala
@@ -0,0 +1,67 @@
+package org.apache.spark.scheduler.cluster
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.hive.HiveContext
+import org.scalatest.Matchers
+import org.scalatest.FlatSpec
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class PartitionDistributorTest extends FlatSpec with Matchers {
+
+   val conf = new SparkConf
+   conf.set("spark.app.name", "Kashish")
+   conf.set("spark.executor.instances", "4")
+   val query = "select pop_id from telstra_database_9_pop_segment_mime_hourly_out___default_binsrc___60 where timestamp>=1401253200 and timestamp<=1401260400"
+   println("SQL is " + query)
+   println("num of executors " + 4)
+   val sc = new SparkContext(conf)
+   val hiveContext = new HiveContext(sc)
+   val hiveRdd = hiveContext.sql(query)
+   val numPartitions = hiveRdd.partitions.size
+   println("NumPartitons = " + numPartitions)
+   var partitonDistributorRDD = new PartitonDistributorRDD(sc, numPartitions)
+   partitonDistributorRDD = partitonDistributorRDD.cache
+  
+//  override def beforeEach {
+//     
+//  }
+  
+  "PartitionDistributorTest" should " start without Exception " in {
+    
+    val zipped = partitonDistributorRDD.zipRdd(hiveRdd)
+	println("Caching zipped Partition")
+	zipped.cache
+	
+	println("Printing zippedRdd")
+	zipped.collect.foreach(println)
+	
+	this.synchronized {
+		this.wait()
+	}
+    
+  }
+   
+  "PartitionDistributorTest2" should " start without Exception " in {
+    
+    val zipped = partitonDistributorRDD.zipRdd(hiveRdd)
+	println("Caching zipped Partition")
+	zipped.cache
+	
+	println("Printing zippedRdd")
+	zipped.collect.foreach(println)
+	
+	this.synchronized {
+		this.wait()
+	}
+
+  }
+  
+//  override def afterEach {
+//    deleteOutFile("src/test/resources/CloneXmlTest/test-clone/out",false)
+//  }
+
+  
+}
