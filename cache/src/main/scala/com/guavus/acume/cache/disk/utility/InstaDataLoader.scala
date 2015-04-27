3d28bfbaa1eec9c5b53d7f3af057998541d3ee8a
pankaj.arora <pankaj.arora@guavus.com>
2015-04-27 11:28:55 +0530

    ACU-389: Added logic to prioritise cube selection based on cube name
    Former-commit-id: a53f90c01f4e0c0c7631d287b39c0eda0288a2aa

--- cache/src/main/scala/com/guavus/acume/cache/disk/utility/InstaDataLoader.scala
+++ cache/src/main/scala/com/guavus/acume/cache/disk/utility/InstaDataLoader.scala
@@ -194,9 +194,11 @@ class InstaDataLoader(@transient acumeCacheContext: AcumeCacheContextTrait, @tra
       }
       if (isValidCubeGran && cube.binSource == businessCube.binsource && CubeUtil.getCubeBaseFields(businessCube).toSet.subsetOf((cube.dimensions.map(_._1) ++ cube.measures.map(_._1)).toSet)) {
         if (bestCube == null) {
-          bestCube = cube
+          return cube
         } else {
-          if (bestCube.dimensions.size > cube.dimensions.size) {
+          if (cube.cubeName.equalsIgnoreCase(businessCube.cubeName)) {
+            bestCube = cube
+          } else if (bestCube.dimensions.size > cube.dimensions.size) {
             bestCube = cube
           } else if (bestCube.dimensions.size == cube.dimensions.size) {
             if (bestCube.measures.size > cube.measures.size) {
