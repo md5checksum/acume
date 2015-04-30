b4050829e3dd78df24eb7552cbf5cf2fcbaa3b41
pankaj.arora <pankaj.arora@guavus.com>
2015-04-30 12:46:33 +0530

    ACU-448: Added support for thin client to use insta api for using aggregation intervals
    Conflicts:
    	cache/pom.xml
    	cache/src/main/scala/com/guavus/acume/cache/common/CacheLevel.scala
    	cache/src/main/scala/com/guavus/acume/cache/common/ConfConstants.scala
    	cache/src/main/scala/com/guavus/acume/cache/common/Cube.scala
    	cache/src/main/scala/com/guavus/acume/cache/common/LevelTimestamp.scala
    	cache/src/main/scala/com/guavus/acume/cache/core/AcumeCache.scala
    	cache/src/main/scala/com/guavus/acume/cache/core/AcumeCacheFactory.scala
    	cache/src/main/scala/com/guavus/acume/cache/core/AcumeFlatSchemaTreeCache.scala
    	cache/src/main/scala/com/guavus/acume/cache/core/AcumeStarSchemaTreeCache.scala
    	cache/src/main/scala/com/guavus/acume/cache/core/AcumeTreeCache.scala
    	cache/src/main/scala/com/guavus/acume/cache/core/AcumeTreeCacheValue.scala
    	cache/src/main/scala/com/guavus/acume/cache/core/SingleEntityAcumeTreeCache.scala
    	cache/src/main/scala/com/guavus/acume/cache/disk/utility/CubeUtil.scala
    	cache/src/main/scala/com/guavus/acume/cache/disk/utility/DataLoader.scala
    	cache/src/main/scala/com/guavus/acume/cache/disk/utility/InstaDataLoader.scala
    	cache/src/main/scala/com/guavus/acume/cache/eviction/AcumeCacheEvictionObserver.scala
    	cache/src/main/scala/com/guavus/acume/cache/eviction/AcumeTreeCacheEvictionPolicy.scala
    	cache/src/main/scala/com/guavus/acume/cache/eviction/EvictionPolicy.scala
    	cache/src/main/scala/com/guavus/acume/cache/sql/AcumeCacheSQLCorrector.scala
    	cache/src/main/scala/com/guavus/acume/cache/sql/ISqlCorrector.scala
    	cache/src/main/scala/com/guavus/acume/cache/utility/TimeZoneInfo.scala
    	cache/src/main/scala/com/guavus/acume/cache/utility/Utility.scala
    	cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeCacheContext.scala
    	cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeCacheContextTrait.scala
    	cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeHiveCacheContext.scala
    	cache/src/main/scala/com/guavus/acume/cache/workflow/RequestResponseCache.scala
    	core/src/main/scala/com/guavus/acume/core/converter/AcumeDataSourceSchema.scala
    
    Former-commit-id: cfd02db8eae602f433304bd6ae252bd1503abe90

--- cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeHiveCacheContext.scala
+++ cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeHiveCacheContext.scala
@@ -8,6 +8,18 @@ import com.guavus.acume.cache.common.AcumeCacheConf
 import com.guavus.acume.cache.common.ConfConstants
 import com.guavus.acume.cache.common.QLType
 import com.guavus.acume.cache.utility.Utility
+import com.guavus.acume.cache.sql.ISqlCorrector
+import scala.collection.mutable.ArrayBuffer
+import com.guavus.acume.cache.common.Cube
+import scala.collection.JavaConversions._
+import com.guavus.acume.cache.disk.utility.DataLoader
+import java.util.concurrent.ConcurrentHashMap
+import com.guavus.acume.cache.common.Cube
+import com.guavus.acume.cache.disk.utility.InstaDataLoaderThinAcume
+import com.guavus.acume.cache.common.BaseCube
+import com.guavus.acume.cache.common.LevelTimestamp
+import com.guavus.acume.cache.common.CacheLevel
+import com.guavus.acume.cache.core.FixedLevelPolicy
 
 /**
  * @author kashish.jain
@@ -20,9 +32,13 @@ class AcumeHiveCacheContext(val sqlContext: SQLContext, val conf: AcumeCacheConf
     case sqlContext: SQLContext => 
     case rest => throw new RuntimeException("This type of SQLContext is not supported.")
   }
-  
+  Utility.init(conf)
   Utility.unmarshalXML(conf.get(ConfConstants.businesscubexml), dimensionMap, measureMap)
   
+  val dataLoader: DataLoader = new InstaDataLoaderThinAcume(this, conf, null)
+
+  override private [cache] val dataloadermap = new ConcurrentHashMap[String, DataLoader]
+
   private [acume] def cacheSqlContext() : SQLContext = sqlContext
   
   private [acume] def cacheConf = conf
@@ -30,10 +46,65 @@ class AcumeHiveCacheContext(val sqlContext: SQLContext, val conf: AcumeCacheConf
   private [acume] def getCubeMap = throw new RuntimeException("Operation not supported")
   
   private [acume] def executeQuery(sql: String, qltype: QLType.QLType) = {
-    val resultSchemaRDD = sqlContext.sql(sql)
-    new AcumeCacheResponse(resultSchemaRDD, resultSchemaRDD, MetaData(-1, Nil))
+    if(!cacheConf.getBoolean(ConfConstants.useInsta, false)) {
+      val resultSchemaRdd = sqlContext.sql(sql)
+      new AcumeCacheResponse(resultSchemaRdd, resultSchemaRdd, new MetaData(-1, Nil))
+    } else {
+    val originalparsedsql = AcumeCacheContext.parseSql(sql)
+    
+    println("AcumeRequest obtained " + sql)
+    var correctsql = ISqlCorrector.getSQLCorrector(conf).correctSQL(this, sql, (originalparsedsql._1.toList, originalparsedsql._2))
+    var updatedsql = correctsql._1._1
+    val queryOptionalParams = correctsql._1._2
+    var updatedparsedsql = correctsql._2
+    
+    val rt = updatedparsedsql._2
+      
+    var i = ""
+      var timestamps = scala.collection.mutable.MutableList[Long]() 
+    val list = for(l <- updatedparsedsql._1) yield {
+      val cube = l.getCubeName
+      val binsource = l.getBinsource
+      val startTime = l.getStartTime
+      val endTime = l.getEndTime
+    
+      val key_binsource = 
+        if(binsource != null)
+          binsource
+      else
+        conf.get(ConfConstants.acumecorebinsource)
+
+      i = AcumeCacheContext.getTable(cube)
+      updatedsql = updatedsql.replaceAll(s"$cube", s"$i")
+      val finalRdd = if(rt == RequestType.Timeseries) {
+        val level = queryOptionalParams.getTimeSeriesGranularity
+    	  val startTimeCeiling = Utility.floorFromGranularity(startTime, level)
+    	  val endTimeFloor = Utility.floorFromGranularity(endTime, level)
+           timestamps = Utility.getAllIntervals(startTimeCeiling, endTimeFloor, level)
+          val tables = for(timestamp <- timestamps) yield {
+        	  val rdd = dataLoader.loadData(Map[String, Any](), new BaseCube(cube, binsource, null, null, null), timestamp, Utility.getNextTimeFromGranularity(timestamp, level, Utility.newCalendar), level)
+        	  val tempTable = AcumeCacheContext.getTable(cube)
+        	  rdd.registerTempTable(tempTable)
+        	  val tempTable1 = AcumeCacheContext.getTable(cube)
+        	  sqlContext.sql(s"select *, $timestamp as ts from $tempTable").registerTempTable(tempTable1)
+        	  tempTable1
+          }
+        val finalQuery = tables.map(x => s" select * from $x ").mkString(" union all ")
+          sqlContext.sql(finalQuery)
+      } else {
+    	  val rdd = dataLoader.loadData(Map[String, Any](), new BaseCube(cube, binsource, null, null, null), startTime, endTime, 0l)
+    	  val tempTable = AcumeCacheContext.getTable(cube)
+        	  rdd.registerTempTable(tempTable)
+        	  sqlContext.sql(s"select *, $startTime as ts from $tempTable")
+      }
+      print("Registering Temp Table " + i)
+      finalRdd.registerTempTable(i)
+    }
+    print("Thin Query " + updatedsql)
+    val resultSchemaRDD = sqlContext.sql(updatedsql)
+    new AcumeCacheResponse(resultSchemaRDD, resultSchemaRDD, MetaData(-1, timestamps.toList))
   }
-  
+  }
 }
 
 object AcumeHiveCacheContext{
