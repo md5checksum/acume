327788f5fd4f2c0c72c7414db0114d9a7bd6ded9
Kashish Jain <kashish.jain@guavus.com>
2015-05-07 15:24:37 +0530

    SPX-386: [Revert] Integrating acume cache with partition distributor
    Former-commit-id: 9971ec01e34f83f39b1664440b1fed2dc4045acb

--- cache/src/main/scala/com/guavus/acume/cache/core/AcumeTreeCacheValue.scala
+++ cache/src/main/scala/com/guavus/acume/cache/core/AcumeTreeCacheValue.scala
@@ -1,25 +1,8 @@
 package com.guavus.acume.cache.core
 
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.SchemaRDD
-import org.apache.spark.scheduler.cluster.PartitionDistributorRDD
 
-class AcumeTreeCacheValue(val dimensionTableName: String, val measuretableName: String, var measureschemardd: SchemaRDD) {
-  
-  def this(sqlContext: SQLContext, dimensionTableName: String, measuretableName: String, measureschemardd: SchemaRDD) {
-    
-    this(dimensionTableName, measuretableName, null)
-    val numPartitions = measureschemardd.partitions.size
-    val partitionDistributorRdd = PartitionDistributorRDD.getInstance(sqlContext.sparkContext, numPartitions)
-    val zippedSchemaRdd = sqlContext.applySchema(partitionDistributorRdd.zipRdd(measureschemardd), measureschemardd.schema)
- 
-    zippedSchemaRdd.registerTempTable(measuretableName)
-    this.measureschemardd = zippedSchemaRdd
-    sqlContext.cacheTable(measuretableName)
-  }
- 
-}
-
+case class AcumeTreeCacheValue(val dimensionTableName: String, val measuretableName: String, var measureschemardd: SchemaRDD)
 
 
 
