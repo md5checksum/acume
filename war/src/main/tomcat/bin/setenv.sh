4b674d51bc4101a467ffbccf0b8809a92e94e561
Shreyans Tyagi <shreyans.tyagi@guavus.com>
2015-05-27 17:25:33 +0530

    SPX-494: GC related properties added to setenv.sh
    Former-commit-id: 73a961f7a71057208658638b259ccbce0009dd52

--- war/src/main/tomcat/bin/setenv.sh
+++ war/src/main/tomcat/bin/setenv.sh
@@ -4,6 +4,19 @@ else
         DEBUG_OPTS=" -Xdebug -Xrunjdwp:transport=dt_socket,address=$CLI_REPLACE_DEBUGPORT$,server=y,suspend=n"
 fi
 
+NUM_GC_LOG_FILES=5
+NOW=`date +"%Y-%m-%d.%H-%M-%S"`
+for (( i=0; i<$NUM_GC_LOG_FILES; i++ ))
+  do
+    if [ -s gc.log.$i ]; then
+      mv gc.log.$i gc.$NOW.$i.log
+    elif [ -f gc.log.$i ]; then
+      rm gc.log.$i
+    fi
+  done
+
+GC_OPTS=" -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseGCLogFileRotation -XX:GCLogFileSize=500M -XX:NumberOfGCLogFiles=$NUM_GC_LOG_FILES"
+
 ACUME_PERMGEN_CONFIG=" -XX:PermSize=$CLI_REPLACE_PERMSIZE$ -XX:MaxPermSize=$CLI_REPLACE_MAXPERMSIZE$ "
-export ACUME_JAVA_OPTS=$DEBUG_OPTS$ACUME_PERMGEN_CONFIG
+export ACUME_JAVA_OPTS=$DEBUG_OPTS$ACUME_PERMGEN_CONFIG$GC_OPTS
 
