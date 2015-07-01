ffeb1c5f5ff30396e6fb0cd00b20a2f32eeade23
shashank.jain <shashank.jain@guavus.com>
2015-07-01 15:43:36 +0530

    TCP-384: remove the extra statement
    Former-commit-id: 5aac98dd5c95bf12bdd2ae897f26890c3854f1aa

--- core/src/main/scala/com/guavus/acume/core/DataService.scala
+++ core/src/main/scala/com/guavus/acume/core/DataService.scala
@@ -279,7 +279,6 @@ class DataService(queryBuilderService: Seq[IQueryBuilderService], val acumeConte
         logger.error("Cancelling Query " + sql + " with GroupId " + jobGroupId + " due to timeout.", e)
         acumeContext.sc.cancelJobGroup(jobGroupId)
         throw new AcumeException(AcumeExceptionConstants.TIMEOUT_EXCEPTION.name);
-       case e: Throwable =>
       case e: Throwable =>
         logger.error("Cancelling Query " + sql + " with GroupId " + jobGroupId, e)
         acumeContext.sc.cancelJobGroup(jobGroupId)
