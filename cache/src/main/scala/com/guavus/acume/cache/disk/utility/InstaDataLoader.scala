50c6b06acdbc8a199bbe188ae1403e1a78898929
Kashish Jain <kashish.jain@guavus.com>
2015-07-09 19:54:24 +0530

    ACU-689: Fixing instaDataloader to query at fixed interval of time
    Conflicts:
    	cache/src/main/scala/com/guavus/acume/cache/disk/utility/InstaDataLoader.scala
    
    Former-commit-id: ea18ff49a50ac63815e2e30f6a22303a4d4c5372

--- cache/src/main/scala/com/guavus/acume/cache/disk/utility/InstaDataLoader.scala
+++ cache/src/main/scala/com/guavus/acume/cache/disk/utility/InstaDataLoader.scala
@@ -1,40 +1,28 @@
 package com.guavus.acume.cache.disk.utility
 
-import java.util.Arrays
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+
+import scala.annotation.migration
 import scala.util.control.Breaks._
-import org.apache.spark.AccumulatorParam
-import org.apache.spark.rdd.RDD
+
 import org.apache.spark.sql.SchemaRDD
-import org.apache.spark.sql.StructField
-import org.apache.spark.sql.StructType
-import org.apache.spark.sql.catalyst.expressions.GenericRow
-import org.apache.spark.sql.catalyst.expressions.Row
-import org.apache.spark.sql.catalyst.types.LongType
+import org.apache.spark.sql.hive.HiveContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 import com.guavus.acume.cache.common.AcumeCacheConf
 import com.guavus.acume.cache.common.ConfConstants
-import com.guavus.acume.cache.common.ConversionToSpark
 import com.guavus.acume.cache.common.Cube
-import com.guavus.acume.cache.common.DimensionTable
 import com.guavus.acume.cache.common.LevelTimestamp
 import com.guavus.acume.cache.core.AcumeCache
 import com.guavus.acume.cache.utility.Utility
 import com.guavus.acume.cache.workflow.AcumeCacheContext
 import com.guavus.acume.cache.workflow.AcumeCacheContextTrait
-import com.guavus.acume.cache.workflow.AcumeCacheContextTrait
-import com.guavus.insta.BinPersistTimeInfoRequest
-import com.guavus.insta.BinPersistTimeInfoRequest
 import com.guavus.insta.Insta
 import com.guavus.insta.InstaCubeMetaInfo
 import com.guavus.insta.InstaRequest
-import scala.collection.mutable.HashMap
-import com.google.common.cache.CacheBuilder
-import com.google.common.cache.CacheLoader
-import java.util.concurrent.TimeUnit
-import org.apache.spark.sql.hive.HiveContext
-import com.google.common.util.concurrent.ListenableFuture
-import com.google.common.util.concurrent.Futures
-import com.guavus.acume.cache.common.CubeTrait
 import com.guavus.acume.cache.common.CubeTrait
 
 class InstaDataLoader(@transient acumeCacheContext: AcumeCacheContextTrait, @transient  conf: AcumeCacheConf, @transient acumeCache: AcumeCache[_ <: Any, _ <: Any]) extends DataLoader(acumeCacheContext, conf, null) {
@@ -42,44 +30,48 @@ class InstaDataLoader(@transient acumeCacheContext: AcumeCacheContextTrait, @tra
   @transient var insta: Insta = null
   @transient val sqlContext = acumeCacheContext.cacheSqlContext
   @transient var cubeList: List[InstaCubeMetaInfo] = null
-  val binSourceToIntervalMap = CacheBuilder.newBuilder().refreshAfterWrite(acumeCacheContext.cacheConf.getInt(ConfConstants.instaAvailabilityPollInterval), TimeUnit.SECONDS)
-    .build(
-      new CacheLoader[String, Map[String, Map[Long, (Long, Long)]]]() {
-        var future: com.google.common.util.concurrent.ListenableFuture[Map[String,Map[Long,(Long, Long)]]] = null
-        def load(key: String): Map[String, Map[Long, (Long, Long)]] = {
-          val persistTime = insta.getAllBinPersistedTimes
-          println(persistTime)
-          persistTime.map(binSourceToGranToAvailability => {
-            val minGran = binSourceToGranToAvailability._2.filter(_._1 != -1).keys.min
-            val granularityToAvailability = binSourceToGranToAvailability._2.map(granToAvailability => {
-              if (granToAvailability._1 == -1) {
-                (granToAvailability._1, (granToAvailability._2._1, Utility.getNextTimeFromGranularity(granToAvailability._2._2, minGran, Utility.newCalendar)))
-              } else {
-                (granToAvailability._1, (granToAvailability._2._1, Utility.getNextTimeFromGranularity(granToAvailability._2._2, granToAvailability._1, Utility.newCalendar)))
-              }
-            })
-            (binSourceToGranToAvailability._1, granularityToAvailability ++ Map(-1L -> granularityToAvailability.get(minGran).get))
-          })
-        }
 
-        override def reload(key: String, oldValue: Map[String, Map[Long, (Long, Long)]]): ListenableFuture[Map[String, Map[Long, (Long, Long)]]] = {
-          if(future == null) {
-        	  future = Futures.immediateFuture(load(key))
-        	  Futures.immediateFuture(oldValue);
-          }
-          if (future.isDone()) {
-            val returnFuture = future
-            future = null
-            returnFuture
-          } else {
-            Futures.immediateFuture(oldValue);
-          }
-        }
-
-      });
-  init
+  @transient private val logger: Logger = LoggerFactory.getLogger(classOf[InstaDataLoader])
+  private var binSourceToIntervalMap = Map[String, Map[Long, (Long, Long)]]()
+  @transient private val lockObject = new Object
   
-  def init() {
+  // Initialize the insta client
+  init
+    
+  val runnable = new Runnable() {
+    def run() {
+      synchronizedGetAndUpdateMap
+    }
+  };
+  
+  private val service : ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
+  service.scheduleAtFixedRate(runnable, 0, acumeCacheContext.cacheConf.getInt(ConfConstants.instaAvailabilityPollInterval), TimeUnit.SECONDS);
+
+  private def synchronizedGetAndUpdateMap() {
+   lockObject.synchronized {
+     binSourceToIntervalMap = getUpdatedBinSourceToIntervalMap
+   }
+  }
+  
+  private def getUpdatedBinSourceToIntervalMap() = {
+    val persistTime = insta.getAllBinPersistedTimes
+    println(persistTime)
+    val updatedBinSourceToIntervalMap = persistTime.map(binSourceToGranToAvailability => {
+      val minGran = binSourceToGranToAvailability._2.filter(_._1 != -1).keys.min
+      val granularityToAvailability = binSourceToGranToAvailability._2.map(granToAvailability => {
+        if (granToAvailability._1 == -1) {
+          (granToAvailability._1, (granToAvailability._2._1, Utility.getNextTimeFromGranularity(granToAvailability._2._2, minGran, Utility.newCalendar)))
+        } else {
+          (granToAvailability._1, (granToAvailability._2._1, Utility.getNextTimeFromGranularity(granToAvailability._2._2, granToAvailability._1, Utility.newCalendar)))
+        }
+      })
+      (binSourceToGranToAvailability._1, granularityToAvailability ++ Map(-1L -> granularityToAvailability.get(minGran).get))
+    })
+    
+    updatedBinSourceToIntervalMap
+  }
+  
+  private def init() {
     //Insta constructor requires HiveContext now, so we have to do explicit typecasting here
     insta = new Insta(acumeCacheContext.cacheSqlContext.asInstanceOf[HiveContext])
 //    insta.init(conf.get(ConfConstants.backendDbName, throw new IllegalArgumentException(" Insta DBname is necessary for loading data from insta")), conf.get(ConfConstants.cubedefinitionxml, throw new IllegalArgumentException(" Insta cubeDefinitionxml is necessary for loading data from insta")))
@@ -174,7 +166,6 @@ class InstaDataLoader(@transient acumeCacheContext: AcumeCacheContextTrait, @tra
     }
   }
   
-  
   def getBestCubeName(businessCube: CubeTrait, startTime: Long, endTime: Long): InstaCubeMetaInfo = {
     val fieldnum = Integer.MAX_VALUE;
     var resultantTable = null;
@@ -236,7 +227,11 @@ class InstaDataLoader(@transient acumeCacheContext: AcumeCacheContextTrait, @tra
 		  getAllBinSourceToIntervalMap.getOrElse(binSource, throw new IllegalArgumentException("No Data found for binSource " +  binSource))
   }
   
-  override def getAllBinSourceToIntervalMap() : Map[String,Map[Long, (Long,Long)]] =  {
-    binSourceToIntervalMap.get("binSourceToIntervalMap")
+  override def getAllBinSourceToIntervalMap() : Map[String, Map[Long, (Long,Long)]] =  {
+    if(binSourceToIntervalMap.isEmpty) {
+      synchronizedGetAndUpdateMap
+      // This means its happening for the first time
+    }
+    binSourceToIntervalMap
   }
 }
