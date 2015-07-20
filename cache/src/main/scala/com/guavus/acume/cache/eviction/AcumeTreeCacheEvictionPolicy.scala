3627635ee28668d0b89040f7c36bb27e4c74cf97
Kashish Jain <kashish.jain@guavus.com>
2015-07-20 17:23:15 +0530

    ACU-001: FIxing compilation
    Former-commit-id: ec67a22f529483ec28eaf323e72888d3e3083094

--- cache/src/main/scala/com/guavus/acume/cache/eviction/AcumeTreeCacheEvictionPolicy.scala
+++ cache/src/main/scala/com/guavus/acume/cache/eviction/AcumeTreeCacheEvictionPolicy.scala
@@ -23,8 +23,6 @@ import org.slf4j.Logger
  */
 
 class AcumeTreeCacheEvictionPolicy(cube: Cube, cacheContext : AcumeCacheContextTrait) extends EvictionPolicy(cube, cacheContext) {
-  
-  private var logger: Logger = LoggerFactory.getLogger(classOf[AcumeTreeCacheEvictionPolicy])
 
   private val logger: Logger = LoggerFactory.getLogger(classOf[AcumeTreeCacheEvictionPolicy])
   
