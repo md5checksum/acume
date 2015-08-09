189d6f22f2fc724408d32832b52540c1e4ba3996
pankaj.arora <pankaj.arora@guavus.com>
2015-08-10 01:54:01 +0530

    ACU-754: Fixed type cast in interpreter policy
    Former-commit-id: fb581ca72565115ee4a3539d4df7b506038aa908

--- core/src/main/scala/com/guavus/acume/core/DsInterpreterPolicy.scala
+++ core/src/main/scala/com/guavus/acume/core/DsInterpreterPolicy.scala
@@ -1,13 +1,12 @@
 package com.guavus.acume.core
 
 import java.io.StringReader
-
 import com.guavus.acume.cache.common.ConfConstants
 import com.guavus.acume.cache.utility.SQLParserFactory
 import com.guavus.acume.cache.workflow.AcumeCacheContextTraitUtil
-
 import net.sf.jsqlparser.statement.select.PlainSelect
 import net.sf.jsqlparser.statement.select.Select
+import net.sf.jsqlparser.statement.select.PlainSelect
 
 /**
  * @author kashish.jain
@@ -22,7 +21,7 @@ class DsInterpreterPolicyImpl extends DsInterpreterPolicy {
   
   def interpretDsName(query: String) : String = {
     val sql = SQLParserFactory.getParserManager
-    val statement = sql.parse(new StringReader(query)).asInstanceOf[Select].asInstanceOf[PlainSelect]
+    val statement = sql.parse(new StringReader(query)).asInstanceOf[Select].getSelectBody.asInstanceOf[PlainSelect]
     
     val tableName = statement.getInto.getName
     
