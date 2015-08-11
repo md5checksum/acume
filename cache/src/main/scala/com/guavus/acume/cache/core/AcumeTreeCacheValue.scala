420e9b9455bf44cf6353bb5a70b30a7f77d3fae8
Kashish Jain <kashish.jain@guavus.com>
2015-08-11 17:20:51 +0530

    ACU-807: Avoiding the none.get exception
    Former-commit-id: cd9a0271ed82db5bab68384c6ca84c0507a7b914

--- cache/src/main/scala/com/guavus/acume/cache/core/AcumeTreeCacheValue.scala
+++ cache/src/main/scala/com/guavus/acume/cache/core/AcumeTreeCacheValue.scala
@@ -145,7 +145,10 @@ case class AcumeInMemoryValue(levelTimestamp: LevelTimestamp, cube: Cube, measur
       logger.info("Unpersisting Data object {} for temp_memory_only for cube " + cube.getAbsoluteCubeName, levelTimestamp)
       logger.info("Dropping temp tables {}", tempTables.mkString(","))
       evictFromMemory
-      tempTables.get.asInstanceOf[scala.collection.mutable.ArrayBuffer[String]].map(x => measureSchemaRdd.sqlContext.dropTempTable(x))
+      tempTables match {
+        case Some(table) => table.asInstanceOf[scala.collection.mutable.ArrayBuffer[String]].map(x => measureSchemaRdd.sqlContext.dropTempTable(x))
+        case None =>
+      }
       measureSchemaRdd.sqlContext.dropTempTable(tableName)
     } catch {
       case e: Exception => logger.error("", e)
