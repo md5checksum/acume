5b6ba7cbc010f7c128c540ee032637e0e46ac270
Neeru Jaroliya <neeru.jaroliya@guavus.com>
2015-08-12 11:56:00 +0530

    ACU-445:validations issue fixed
    Former-commit-id: 7ca15a0d029053a37cf2a3db11f29d9be9178336

--- cache/src/main/scala/com/guavus/acume/cache/utility/PropertyValidator.scala
+++ cache/src/main/scala/com/guavus/acume/cache/utility/PropertyValidator.scala
@@ -7,133 +7,177 @@ import org.slf4j.LoggerFactory
 import com.guavus.acume.cache.common.ConfConstants
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
+import com.guavus.acume.cache.core.TimeGranularity
 
 case class PropertyValidator()
 
 object PropertyValidator {
-  
+
   private var logger: Logger = LoggerFactory.getLogger(classOf[PropertyValidator])
-  
-  def validate(settings : HashMap[String, String]) = {
+
+  def validate(settings: HashMap[String, String]) = {
     if (validateRetentionMap(settings.get(ConfConstants.schedulerVariableRetentionMap), ConfConstants.schedulerVariableRetentionMap)
-        && validateRetentionMap(settings.get(ConfConstants.acumecorelevelmap), ConfConstants.acumecorelevelmap)
-        && validateTimeSeriesRetentionMap(settings.get(ConfConstants.acumecoretimeserieslevelmap), ConfConstants.acumecoretimeserieslevelmap)
-        && isNumber(settings.get(ConfConstants.rrcacheconcurrenylevel), ConfConstants.rrcacheconcurrenylevel)
-	    && isNumber(settings.get(ConfConstants.rrsize._1), ConfConstants.rrsize._1)
-	    && isNumber(settings.get(ConfConstants.prefetchTaskRetryIntervalInMillis), ConfConstants.prefetchTaskRetryIntervalInMillis)
-	    && isNumber(settings.get(ConfConstants.threadPoolSize), ConfConstants.threadPoolSize)
-	    && isNumber(settings.get(ConfConstants.instaComboPoints), ConfConstants.instaComboPoints)
-	    && isNumber(settings.get(ConfConstants.variableRetentionCombinePoints), ConfConstants.variableRetentionCombinePoints)
-	    && isNumber(settings.get(ConfConstants.queryPrefetchTaskNoOfRetries), ConfConstants.queryPrefetchTaskNoOfRetries)
-	    && isNumber(settings.get(ConfConstants.maxSegmentDuration), ConfConstants.maxSegmentDuration)
-	    && isNumber(settings.get(ConfConstants.maxQueryLogRecords), ConfConstants.maxQueryLogRecords)
-	    && isNumber(settings.get(ConfConstants.schedulerCheckInterval), ConfConstants.schedulerCheckInterval)
-	    && isBoolean(settings.get(ConfConstants.enableJDBCServer), ConfConstants.enableJDBCServer)
-	    && isBoolean(settings.get(ConfConstants.enableScheduler), ConfConstants.enableScheduler))
-    {
+      && validateRetentionMap(settings.get(ConfConstants.acumecorelevelmap), ConfConstants.acumecorelevelmap)
+      && validateTimeSeriesRetentionMap(settings.get(ConfConstants.acumecoretimeserieslevelmap), ConfConstants.acumecoretimeserieslevelmap)
+      && isNumber(settings.get(ConfConstants.rrcacheconcurrenylevel), ConfConstants.rrcacheconcurrenylevel)
+      && isNumber(settings.get(ConfConstants.rrsize._1), ConfConstants.rrsize._1)
+      && isNumber(settings.get(ConfConstants.prefetchTaskRetryIntervalInMillis), ConfConstants.prefetchTaskRetryIntervalInMillis)
+      && isNumber(settings.get(ConfConstants.threadPoolSize), ConfConstants.threadPoolSize)
+      && isNumber(settings.get(ConfConstants.instaComboPoints), ConfConstants.instaComboPoints)
+      && isNumber(settings.get(ConfConstants.variableRetentionCombinePoints), ConfConstants.variableRetentionCombinePoints)
+      && isNumber(settings.get(ConfConstants.queryPrefetchTaskNoOfRetries), ConfConstants.queryPrefetchTaskNoOfRetries)
+      && isNumber(settings.get(ConfConstants.maxSegmentDuration), ConfConstants.maxSegmentDuration)
+      && isNumber(settings.get(ConfConstants.maxQueryLogRecords), ConfConstants.maxQueryLogRecords)
+      && isNumber(settings.get(ConfConstants.schedulerCheckInterval), ConfConstants.schedulerCheckInterval)
+      && isBoolean(settings.get(ConfConstants.enableJDBCServer), ConfConstants.enableJDBCServer)
+      && isBoolean(settings.get(ConfConstants.enableScheduler), ConfConstants.enableScheduler)) {
       logger.info("Valid properties")
     } else {
       throw new RuntimeException("Invalid acume properties...")
     }
   }
-  
-  def isBoolean(value : Option[String], key : String = "Key") : Boolean = {
-    if(value == None) {
+
+  def isBoolean(value: Option[String], key: String = "Key"): Boolean = {
+    if (value == None) {
       logger.error(key + " is not configured in acume conf")
       return false
     }
-    if(!value.get.toLowerCase().matches("true|false")) {
+    if (!value.get.toLowerCase().matches("true|false")) {
       logger.error(key + " is not a boolean")
       return false
     }
     true
   }
-  
-  def isNumber(value : Option[String], key: String = "Key") : Boolean = {
-    if(value == None) {
+
+  def isNumber(value: Option[String], key: String = "Key"): Boolean = {
+    if (value == None) {
       logger.error(key + " is not configured in acume conf")
       return false
     }
-    if(!value.get.matches("\\d*")) {
+    if (!value.get.matches("\\d*")) {
       logger.error(key + " is not a number")
       return false
     }
     true
   }
-  
-  def validateTimeSeriesRetentionMap(value : Option[String], key : String = "Key") : Boolean = {
-    if(value == None) {
+
+  def validateTimeSeriesRetentionMap(value: Option[String], key: String = "Key"): Boolean = {
+    if (value == None) {
       logger.error(key + " is not configured in acume conf")
       return false
     }
     val entries = value.get.split(";")
-    if(entries.length == 0) {
+    if (entries.length == 0) {
       logger.error("Length of " + key + " is invalid...")
       return false
     }
     entries.foreach(entry => {
       val subentry = entry.split(":")
-      if(subentry.length != 2 || !isNumber(Some(subentry(1)), key)) {
+      if (subentry.length != 2 || !isNumber(Some(subentry(1)), key)) {
+        logger.error("Format of " + key + " is invalid...")
+        return false
+      }
+      if (inValidFormatCheck(subentry(0))) {
         logger.error("Format of " + key + " is invalid...")
         return false
       }
     })
     true
   }
-  
-  def validateRetentionMap(levelPolicy : Option[String], key : String = "Key") : Boolean = {
-    if(levelPolicy == None || levelPolicy.get.trim == "") {
+
+  def validateRetentionMap(levelPolicy: Option[String], key: String = "Key"): Boolean = {
+    if (levelPolicy == None || levelPolicy.get.trim == "") {
       logger.error(key + " is not configured in acume conf")
       return false
     }
-    
     val levelpolicySplits = levelPolicy.get.trim.split("\\|")
     val inMemoryPolicy = levelpolicySplits(0).trim
-    val diskPolicy = 
-      if(levelpolicySplits.size == 1) {
+    val diskPolicy =
+      if (levelpolicySplits.size == 1) {
         inMemoryPolicy
       } else {
         levelpolicySplits(1).trim
       }
-    
-    if(!Some(diskPolicy).exists(_.trim.nonEmpty) || !Some(inMemoryPolicy).exists(_.trim.nonEmpty)) {
+
+    if (!Some(diskPolicy).exists(_.trim.nonEmpty) || !Some(inMemoryPolicy).exists(_.trim.nonEmpty)) {
       return false
+    }
+
+    if (!splitAndFormatCheck(inMemoryPolicy, key)) {
+      logger.error("Format of " + key + "is invalid for memoryPolicy")
+      return false
+    }
+    
+    if (diskPolicy != inMemoryPolicy) {
+      if (!splitAndFormatCheck(diskPolicy, key)) {
+        logger.error("Format of " + key + "is invalid for diskPolicy")
+        return false
+      }
     }
 
     val inMemoryPolicyMap = Utility.getLevelPointMap(inMemoryPolicy)
     val diskPolicyMap = Utility.getLevelPointMap(diskPolicy)
-    
+
     // Check whether disPolicyMap is > than inMemoryPolicyMap
-    for((inMemoryLevel, inMemoryPoints) <- inMemoryPolicyMap) {
+    for ((inMemoryLevel, inMemoryPoints) <- inMemoryPolicyMap) {
       val diskPolicyPoints = diskPolicyMap.get(inMemoryLevel).getOrElse({
         logger.error("DiskPolicyMap doesnt have all the levels configured in cachelevelPolicyMap")
         return false
       })
-      
-      val matches = diskPolicyMap.entrySet().filter(level => {level.getKey().level == inMemoryLevel.level && level.getKey().aggregationLevel == inMemoryLevel.aggregationLevel}).size
-      if(matches == 0) {
+
+      val matches = diskPolicyMap.entrySet().filter(level => { level.getKey().level == inMemoryLevel.level && level.getKey().aggregationLevel == inMemoryLevel.aggregationLevel }).size
+      if (matches == 0) {
         logger.error("DiskPolicyMap aggregationPoints cannot be less than inMemorylevel aggregation points")
         return false
       }
 
-      if(inMemoryPoints < 0) {
+      if (inMemoryPoints < 0) {
         logger.error("Number of points cannot be less than 0")
         return false
       }
-      
-      if(diskPolicyPoints < inMemoryPoints) {
+
+      if (diskPolicyPoints < inMemoryPoints) {
         logger.error("DiskPolicyMap cannot be less than inMemorylevelPolicyMap")
         return false
       }
-    
-      val fraction = inMemoryLevel.aggregationLevel/inMemoryLevel.level
-      if(Math.ceil(fraction).toLong != fraction) {
+
+      val fraction = inMemoryLevel.aggregationLevel / inMemoryLevel.level
+      if (Math.ceil(fraction).toLong != fraction) {
         logger.error("Combining level is not a multiple of base level")
-       return false
+        return false
       }
-      
+
     }
     true
+  }
+
+  def splitAndFormatCheck(policyMap: String, key: String = "key"): Boolean = {
+    val entries = policyMap.split(";")
+
+    if (entries.length == 0) {
+      return false
+    }
+
+    entries.foreach(entry => {
+      val subentry = entry.split(":")
+      if (!(subentry.length == 2 || subentry.length == 3) && !isNumber(Some(subentry(1)), key)) {
+        return false
+      }
+      if (subentry.length != 2) {
+        if (inValidFormatCheck(subentry(0)) || inValidFormatCheck(subentry(2))) {
+          return false
+        }
+      } else {
+        if (inValidFormatCheck(subentry(0))) {
+          return false
+        }
+      }
+    })
+    return true
+  }
+
+  def inValidFormatCheck(subSubEntry: String): Boolean = {
+    return (TimeGranularity.getTimeGranularityForVariableRetentionName(subSubEntry) == None)
   }
 
 }
