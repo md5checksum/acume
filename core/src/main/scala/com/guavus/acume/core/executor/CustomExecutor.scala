812f0c84a71acb715906479cf6119afacb1328c0
Yash Datta <Yash.Datta@guavus.com>
2015-08-27 15:23:25 +0530

    TCP-160: Use AcumeConf in executor, set jobGroupId
    Former-commit-id: 5c60fab594198aa02c56618783459f3fc5a6a2af

--- core/src/main/scala/com/guavus/acume/core/executor/CustomExecutor.scala
+++ core/src/main/scala/com/guavus/acume/core/executor/CustomExecutor.scala
@@ -1,10 +1,12 @@
 package com.guavus.acume.core.executor
 
 import java.util.concurrent.Callable
+import java.util.concurrent.atomic.AtomicLong
 import com.guavus.acume.cache.common.Cube
 import com.guavus.acume.cache.core.{AcumeCache, TimeGranularity}
 import com.guavus.acume.cache.utility.QueryOptionalParam
 import com.guavus.acume.cache.workflow.{AcumeCacheContext, AcumeCacheContextTrait, CubeKey}
+import CustomExecutor._
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -13,7 +15,7 @@ import com.guavus.rubix.logging.util.AcumeThreadLocal
 import com.guavus.acume.user.management.utils.HttpUtils
 import scala.reflect.{BeanProperty, BooleanBeanProperty}
 import com.guavus.rubix.logging.util.AcumeThreadLocal
-import com.guavus.acume.core.{AcumeContextTrait, AcumeService}
+import com.guavus.acume.core.{AcumeConf, AcumeContextTrait, AcumeService}
 import com.guavus.acume.workflow.RequestDataType
 import scala.collection.mutable.HashMap
 
@@ -36,7 +38,6 @@ abstract class CustomExecutor[T](
 
   @BeanProperty
   var callId: String = _
-  
   /* var properties: HashMap[String, Any] = null
   
   def setProperties(property: HashMap[String, Any]) {
@@ -44,10 +45,24 @@ abstract class CustomExecutor[T](
   } */
   
   final def call(): T = {
+
     var response: T = null.asInstanceOf[T]
+    val acumeContext = AcumeService.dataService.acumeContext
+
     val LoggingInfoWrapper = new LoggingInfoWrapper()
     LoggingInfoWrapper.setTransactionId(this.callId)
+
+    val jobDescription =  Array[String]("[" + HttpUtils.getLoginInfo() + "]").mkString("-")
+
+    logger.info(jobDescription)
+
+    val jobGroupId = Thread.currentThread().getName() + "-" +
+      Thread.currentThread().getId() + "-" + AcumeService.dataService.counter.getAndIncrement
+
+    AcumeConf.setConf(acumeContext.acumeConf)
+
     AcumeThreadLocal.set(LoggingInfoWrapper)
+    acumeContext.sc.setJobGroup(jobGroupId, jobDescription, false)
     try {
 
       // Get cache instance
@@ -61,6 +76,11 @@ abstract class CustomExecutor[T](
 			// execute custom processing part
 			response = customExec(rdds, timeStampList, instance.cube)
 
+    } catch {
+      case e: Throwable =>
+        logger.error(s"Cancelling Query for with GroupId " + jobGroupId, e)
+        acumeContext.sc.cancelJobGroup(jobGroupId)
+        throw e;
     } finally {
       HttpUtils.recycle()
       AcumeThreadLocal.unset()
