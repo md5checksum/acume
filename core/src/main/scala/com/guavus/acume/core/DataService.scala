2c2b0cde22b28471baeaf739be89d97d4e0216ff
Kashish Jain <kashish.jain@guavus.com>
2015-08-31 12:16:50 +0530

    ACU-855: Support multiple datatypes of ts instead of just String
    Former-commit-id: 3ebb3e7487cb422fbe0830af06e98e82ed6b0b4a

--- core/src/main/scala/com/guavus/acume/core/DataService.scala
+++ core/src/main/scala/com/guavus/acume/core/DataService.scala
@@ -40,6 +40,7 @@ import java.util.concurrent.ConcurrentHashMap
 import com.guavus.acume.cache.workflow.AcumeCacheContextTraitUtil
 import DataService._
 import com.guavus.acume.core.configuration.DataServiceFactory
+import com.guavus.acume.cache.common.DataType
 
 
 /**
@@ -217,11 +218,13 @@ class DataService(queryBuilderService: Seq[IQueryBuilderService], val acumeConte
         j += 1
       }
       if (isTimeseries) {
+
         val sortedRows = rows.sortBy(row => row(tsIndex).toString)
         val timestamps : List[Long] = {
+          val tsDataType = AcumeCacheContextTraitUtil.dimensionMap.get("ts").get.getDataType
           val metaDataTimeStamps = cacheResponse.metadata.timestamps
           if(metaDataTimeStamps == null || metaDataTimeStamps.isEmpty)
-            sortedRows.map(row => row(tsIndex).asInstanceOf[String].toLong).toList.distinct
+            sortedRows.map(row => DataType.convertToLong(row(tsIndex), tsDataType)).toList.distinct
           else
             metaDataTimeStamps
         }
