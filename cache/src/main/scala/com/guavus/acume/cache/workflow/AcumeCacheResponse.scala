8299bd322122597d760052e5ea7c9db02ec5e85c
Kashish Jain <kashish.jain@guavus.com>
2015-10-07 11:01:02 +0530

    ACU-956: Optimizing the total records evaluation
    Former-commit-id: 10f5077ef8b28361ef63bf23e279e73f5df6a227

--- cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeCacheResponse.scala
+++ cache/src/main/scala/com/guavus/acume/cache/workflow/AcumeCacheResponse.scala
@@ -1,22 +1,15 @@
 package com.guavus.acume.cache.workflow
 
-import org.apache.spark.sql.SchemaRDD
-import scala.collection.mutable.MutableList
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.Row
 import org.apache.spark.sql.DataFrame
-
+import org.apache.spark.sql.Row
 
 /**
  * @author archit.thakur
  *
  */
 
-case class AcumeCacheResponse(schemaRDD: DataFrame, rowRDD: RDD[Row], metadata:MetaData)
+case class AcumeCacheResponse(var schemaRDD: DataFrame, var rowRDD: RDD[Row], var metadata:MetaData)
 
-
-
-case class MetaData(var totalRecords: Long = 0l, timestamps: List[Long])
-//case class AggregateMetaData(rows: Long) extends MetaData
-//case class TimeSeriesMetaData(timestamps: List[Long]) extends MetaData
+case class MetaData(var totalRecords: Long = 0l, var timestamps: List[Long])
 
